2f7eccf678cc30ebb3491fa9657ef4039ea4b417
FezVrasta <federico.zivolo@gmail.com>
2016-04-05 14:40:18 +0200

    improved loading animation

--- js/main.js
+++ js/main.js
@@ -33,7 +33,6 @@ ga('send', 'pageview');
 			$header = $('#header');
 
 		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
 
 			$window.on('load', function() {
 				window.setTimeout(function() {
