7613284d00dc6aed44e6bb0cf59f619c4081d110
FezVrasta <federico.zivolo@gmail.com>
2016-04-30 16:59:45 +0200

    fixed test on local

--- tests/test-popper.js
+++ tests/test-popper.js
@@ -225,7 +225,7 @@ describe('Popper.js', function() {
             // force redraw
             window.dispatchEvent(new Event('resize'));
 
-            var local = 729;
+            var local = 730;
             var ci = 739;
             expect([local, ci]).toContain(popper.getBoundingClientRect().top);
             expect(popper.getBoundingClientRect().left).toBeApprox(5);
