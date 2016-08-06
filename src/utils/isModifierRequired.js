6e2475a364cd6554ea7029e0bfe99881843918ed
FezVrasta <federico.zivolo@gmail.com>
2016-08-06 16:59:53 +0200

    use filter instead of find to avoid another polyfill

--- src/utils/isModifierRequired.js
+++ src/utils/isModifierRequired.js
@@ -5,12 +5,12 @@
  * @returns {Boolean}
  */
 export default function isModifierRequired(options, requesting, requested) {
-    return options.modifiers.find(modifier => {
+    return !!options.modifiers.filter(modifier => {
         if (modifier.name === requested) {
             return true;
         } else if (modifier.name === requesting) {
             return false;
         }
         return false;
-    }) !== undefined;
+    }).length;
 }
