479a2ceb44c89f4894dfe20813ea360998e84830
FezVrasta <federico.zivolo@gmail.com>
2016-08-11 14:59:46 +0200

    perf optimization

--- src/utils/getOffsetParent.js
+++ src/utils/getOffsetParent.js
@@ -8,5 +8,5 @@
 export default function getOffsetParent(element) {
     // NOTE: 1 DOM access here
     const offsetParent = element.offsetParent;
-    return offsetParent === window.document.body || !offsetParent ? window.document.documentElement : offsetParent;
+    return !offsetParent || offsetParent.nodeName === 'BODY' ? window.document.documentElement : offsetParent;
 }
