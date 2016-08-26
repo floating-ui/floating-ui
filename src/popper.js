30a781e62b1350ccf0faabec6ccee544fccb80fe
Rafael Nunes Verger <rafael@rafaelverger.com.br>
2016-08-26 16:54:37 +0200

    Remove necessity of  polyfill on IE

--- src/popper.js
+++ src/popper.js
@@ -210,7 +210,7 @@
 
         // remove the popper if user explicity asked for the deletion on destroy
         if (this._options.removeOnDestroy) {
-            this._popper.remove();
+            this._popper.parentNode.removeChild(this._popper);
         }
         return this;
     };
