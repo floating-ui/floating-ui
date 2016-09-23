14e635a708af53eb4a02c5c1168567f741a9e605
nadiam84 <nadiam84@gmail.com>
2016-09-23 20:51:45 +1000

    Improve detection strategy of flip modifier (#82)

--- src/modifiers/preventOverflow.js
+++ src/modifiers/preventOverflow.js
@@ -1,4 +1,5 @@
 import getPopperClientRect from '../utils/getPopperClientRect';
+import getOppositePlacement from '../utils/getOppositePlacement';
 
 /**
  * Modifier used to make sure the popper does not overflows from it's boundaries
@@ -9,34 +10,49 @@ import getPopperClientRect from '../utils/getPopperClientRect';
  * @returns {Object} The data object, properly modified
  */
 export default function preventOverflow(data, options) {
+    function shouldMoveWithTarget(direction) {
+        if (!options.moveWithTarget) {
+            return false;
+        }
+        let placement = data.originalPlacement.split('-')[0];
+
+        if (data.flipped && placement === direction || placement === getOppositePlacement(direction)) {
+            return true;
+        }
+        if (placement !== direction && placement !== getOppositePlacement(direction)) {
+            return true;
+        }
+
+        return false;
+    }
     const order = options.priority;
     const popper = getPopperClientRect(data.offsets.popper);
 
     const check = {
         left() {
             let left = popper.left;
-            if (popper.left < data.boundaries.left) {
+            if (popper.left < data.boundaries.left && !shouldMoveWithTarget('left')) {
                 left = Math.max(popper.left, data.boundaries.left);
             }
             return { left: left };
         },
         right() {
             let left = popper.left;
-            if (popper.right > data.boundaries.right) {
+            if (popper.right > data.boundaries.right && !shouldMoveWithTarget('right')) {
                 left = Math.min(popper.left, data.boundaries.right - popper.width);
             }
             return { left: left };
         },
         top() {
             let top = popper.top;
-            if (popper.top < data.boundaries.top) {
+            if (popper.top < data.boundaries.top && !shouldMoveWithTarget('top')) {
                 top = Math.max(popper.top, data.boundaries.top);
             }
             return { top: top };
         },
         bottom() {
             let top = popper.top;
-            if (popper.bottom > data.boundaries.bottom) {
+            if (popper.bottom > data.boundaries.bottom && !shouldMoveWithTarget('bottom')) {
                 top = Math.min(popper.top, data.boundaries.bottom - popper.height);
             }
             return { top: top };
