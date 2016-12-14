fb4594dba84861ce065739e42aea0baaca63f627
Federico Zivolo <fzivolo@quid.com>
2016-12-14 14:39:56 +0100

    fixed `isModifierRequired` to work with new API

--- src/popper/utils/isModifierRequired.js
+++ src/popper/utils/isModifierRequired.js
@@ -1,16 +1,14 @@
 /**
  * Helper used to know if the given modifier depends from another one.
+ * It checks if the needed modifier is listed and enabled.
  * @method
  * @memberof Popper.Utils
  * @returns {Boolean}
  */
-export default function isModifierRequired(modifiers, requesting, requested) {
-    return !!modifiers.filter(modifier => {
-        if (modifier.name === requested) {
-            return true;
-        } else if (modifier.name === requesting) {
-            return false;
-        }
-        return false;
-    }).length;
+export default function isModifierRequired(modifiers, requestingName, requestedName) {
+    const requesting = modifiers.find(({ name }) => name === requestingName);
+
+    return !!requesting && modifiers.some((modifier) => {
+      return modifier.name === requestedName && modifier.enabled && modifier.order < requesting.order;
+    });
 }
