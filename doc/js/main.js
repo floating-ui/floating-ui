8c56a3ca2cf39698c015a2e0cf9d375d2b0310ee
Federico Zivolo <fzivolo@quid.com>
2016-12-30 16:48:21 +0100

    remove stars tooltip on iOS

--- doc/js/main.js
+++ doc/js/main.js
@@ -107,6 +107,8 @@ ga('send', 'pageview');
 	var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 	if (!iOS) {
 		new Popper(document.getElementById('header-title'), document.getElementById('title-popper'));
-	}
+	} else {
+        document.getElementById('title-popper').remove();
+    }
 
 })(jQuery);
