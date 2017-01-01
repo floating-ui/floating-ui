40b04d7390aaed0572f0484b3314a62fdcc5151b
Federico Zivolo <fzivolo@quid.com>
2017-01-01 20:59:30 +0100

    perf optimization

--- src/popper/utils/getScroll.js
+++ src/popper/utils/getScroll.js
@@ -1,10 +1,11 @@
 export default function getScroll(element, side = 'top') {
-    const html = window.document.documentElement;
-    const scrollingElement = window.document.scrollingElement || html;
-
     const upperSide = side === 'top' ? 'scrollTop' : 'scrollLeft';
+
     if (element.nodeName === 'BODY' || element.nodeName === 'HTML') {
+        const html = window.document.documentElement;
+        const scrollingElement = window.document.scrollingElement || html;
         return scrollingElement[upperSide];
     }
+
     return element[upperSide];
 }
