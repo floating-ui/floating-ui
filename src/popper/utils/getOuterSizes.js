62ffa24851e3252bdf42b474979fa3cf8e419f0f
Federico Zivolo <fzivolo@quid.com>
2017-01-02 15:49:32 +0100

    get rid of DOM manipulations that cause reflows

--- src/popper/utils/getOuterSizes.js
+++ src/popper/utils/getOuterSizes.js
@@ -6,14 +6,6 @@
  * @returns {Object} object containing width and height properties
  */
 export default function getOuterSizes(element) {
-    // NOTE: 1 DOM access here
-    const display = element.style.display;
-    const visibility = element.style.visibility;
-
-    element.style.display = 'block';
-    element.style.visibility = 'hidden';
-
-    // original method
     const styles = window.getComputedStyle(element);
     const x = parseFloat(styles.marginTop) + parseFloat(styles.marginBottom);
     const y = parseFloat(styles.marginLeft) + parseFloat(styles.marginRight);
@@ -21,10 +13,5 @@ export default function getOuterSizes(element) {
         width: element.offsetWidth + y,
         height: element.offsetHeight + x
     };
-
-    // reset element styles
-    element.style.display = display;
-    element.style.visibility = visibility;
-
     return result;
 }
