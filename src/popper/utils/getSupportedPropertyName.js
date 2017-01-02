3c438e1f9ac6c0e704559d1545c2486d73b45382
Federico Zivolo <fzivolo@quid.com>
2017-01-02 16:31:26 +0100

    fixed gpuAcceleration detection

--- src/popper/utils/getSupportedPropertyName.js
+++ src/popper/utils/getSupportedPropertyName.js
@@ -6,14 +6,14 @@
  * @returns {String} prefixed property (camelCase)
  */
 export default function getSupportedPropertyName(property) {
-    const prefixes = ['', 'ms', 'webkit', 'moz', 'o'];
+    const prefixes = [false, 'ms', 'webkit', 'moz', 'o'];
     const upperProp = property.charAt(0).toUpperCase() + property.slice(1);
 
-    prefixes.forEach((prefix) => {
+    for (const prefix of prefixes) {
         const toCheck = prefix ? `${prefix}${upperProp}` : property;
         if (typeof window.document.body.style[toCheck] !== 'undefined') {
             return toCheck;
         }
-    });
+    }
     return null;
 }
