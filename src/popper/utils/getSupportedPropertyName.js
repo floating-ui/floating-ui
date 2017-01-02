abb0871117fcafe9d6aae50d581b3eff2119d72f
Federico Zivolo <fzivolo@quid.com>
2017-01-02 20:55:07 +0100

    for...of isn't supported in IE, replaced with for loop

--- src/popper/utils/getSupportedPropertyName.js
+++ src/popper/utils/getSupportedPropertyName.js
@@ -9,7 +9,8 @@ export default function getSupportedPropertyName(property) {
     const prefixes = [false, 'ms', 'webkit', 'moz', 'o'];
     const upperProp = property.charAt(0).toUpperCase() + property.slice(1);
 
-    for (const prefix of prefixes) {
+    for (let i = 0; i < prefixes.length -1; i++) {
+        const prefix = prefixes[i];
         const toCheck = prefix ? `${prefix}${upperProp}` : property;
         if (typeof window.document.body.style[toCheck] !== 'undefined') {
             return toCheck;
