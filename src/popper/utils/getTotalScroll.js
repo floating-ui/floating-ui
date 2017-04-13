39d9932c990b3d66c27910408cc21edededba15b
Federico Zivolo <fzivolo@quid.com>
2017-04-13 13:55:51 +0200

    Fixed test

--- src/popper/utils/getTotalScroll.js
+++ src/popper/utils/getTotalScroll.js
@@ -2,11 +2,19 @@ import getScrollParent from './getScrollParent';
 import getScroll from './getScroll';
 import getParentNode from './getParentNode';
 import isOffsetContainer from './isOffsetContainer';
+import isFixed from './isFixed';
 
 export default function getTotalScroll(element, parent, side = 'top') {
     const scrollParent = getScrollParent(element);
     let scroll = 0;
-    if (isOffsetContainer(scrollParent.nodeName === 'BODY' ? window.document.documentElement : scrollParent)) {
+    const isParentFixed = isFixed(parent);
+
+    // NOTE: I'm not sure the second line of this check is completely correct, review it if
+    // someone complains about viewport problems in future
+    if (
+        isOffsetContainer(scrollParent.nodeName === 'BODY' ? window.document.documentElement : scrollParent) &&
+        ((parent.contains(scrollParent) && isParentFixed) || !isParentFixed)
+    ) {
         scroll = getScroll(scrollParent, side);
     }
 
