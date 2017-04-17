fe386736bcc94e5d1183d216c83d989206a7e15c
Federico Zivolo <federico.zivolo@gmail.com>
2017-04-17 18:05:58 +0200

    [feature] Added counter/clockwise flip behaviors (fix #167) (#215)

---
+++ src/popper/utils/placements.js
@@ -0,0 +1,19 @@
+const placements = [
+  'auto-start',
+  'auto',
+  'auto-end',
+  'top-start',
+  'top',
+  'top-end',
+  'right-start',
+  'right',
+  'right-end',
+  'bottom-end',
+  'bottom',
+  'bottom-start',
+  'left-end',
+  'left',
+  'left-start',
+];
+
+export default placements;
