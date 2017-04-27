3ec1dae10c3921cbcd4e3e198a667bdf3c187d85
Federico Zivolo <fzivolo@quid.com>
2017-04-27 12:51:27 +0200

    [fix] Make bundle.js work on Windows (fix #243)

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -6,7 +6,7 @@ const { build } = argv;
 
 const rollup = 'rollup -c ./scripts/rollup.config.js';
 const minify = 'node ./scripts/minify.js';
-const gzipped = '$(npm bin)/gzipped';
+const gzipped = 'gzipped';
 
 const bundleFactory = (es5, ext, format) => done => {
   const ES5 = es5 || false;
@@ -14,26 +14,32 @@ const bundleFactory = (es5, ext, format) => done => {
   const FORMAT = format || 'es';
   const TARGET = `${EXT ? EXT + '/' : ''}${build}`;
 
-  exec(`${rollup} --environment BUILD:${build},EXT:"${EXT}",FORMAT:${FORMAT}${ES5 ? ',ES5' : ''}`, error => {
-    if (error) {
-      return console.error(`${TARGET}: ${error}`);
-    }
-    exec(`${minify} -i ./dist/${EXT}/${build}.js -o ./dist/${EXT}/${build}.min.js`, error => {
+  exec(
+    `${rollup} --environment BUILD:${build},EXT:"${EXT}",FORMAT:${FORMAT}${ES5 ? ',ES5' : ''}`,
+    error => {
       if (error) {
-        return console.error(`${build}${EXT}.minify: ${error}`);
+        return console.error(`${TARGET}: ${error}`);
       }
-      console.info(
-        colors.yellow(`${TARGET}`),
-        `\n${execSync(`${gzipped} ./dist/${EXT}/${build}.js`).toString()}`
+      exec(
+        `${minify} -i ./dist/${EXT}/${build}.js -o ./dist/${EXT}/${build}.min.js`,
+        error => {
+          if (error) {
+            return console.error(`${build}${EXT}.minify: ${error}`);
+          }
+          console.info(
+            colors.yellow(`${TARGET}`),
+            `\n${execSync(`${gzipped} ./dist/${EXT}/${build}.js`).toString()}`
+          );
+          console.info(
+            colors.yellow(`${TARGET}.min`),
+            `\n${execSync(`${gzipped} ./dist/${EXT}/${build}.min.js`).toString()}`
+          );
+          done();
+        }
       );
-      console.info(
-        colors.yellow(`${TARGET}.min`),
-        `\n${execSync(`${gzipped} ./dist/${EXT}/${build}.min.js`).toString()}`
-      );
-      done();
-    });
-  });
-}
+    }
+  );
+};
 
 const umd_es5 = bundleFactory(true, 'umd', 'umd');
 const esm_es5 = bundleFactory(true, 'esm', 'es');
