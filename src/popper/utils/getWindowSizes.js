2c0f0e538651091471c19809b5998b279b5d369e
Federico Zivolo <fzivolo@quid.com>
2017-05-12 18:26:21 +0200

    [fix] Don't attempt to use scrollSize to detect window sizes
    Because we want the actual window size, not the document size.

--- src/popper/utils/getWindowSizes.js
+++ src/popper/utils/getWindowSizes.js
@@ -1,20 +1,25 @@
+import isIE10 from './isIE10';
+
+function getSize(axis, body, html, computedStyle) {
+  return Math.max(
+    body[`offset${axis}`],
+    html[`client${axis}`],
+    html[`offset${axis}`],
+    isIE10()
+      ? html[`offset${axis}`] +
+          computedStyle[`margin${axis === 'Height' ? 'Top' : 'Left'}`] +
+          computedStyle[`margin${axis === 'Height' ? 'Bottom' : 'Right'}`]
+      : 0
+  )
+}
+
 export default function getWindowSizes() {
   const body = window.document.body;
   const html = window.document.documentElement;
+  const computedStyle = isIE10() && window.getComputedStyle(html);
+
   return {
-    height: Math.max(
-      body.scrollHeight,
-      body.offsetHeight,
-      html.clientHeight,
-      html.scrollHeight,
-      html.offsetHeight
-    ),
-    width: Math.max(
-      body.scrollWidth,
-      body.offsetWidth,
-      html.clientWidth,
-      html.scrollWidth,
-      html.offsetWidth
-    ),
+    height: getSize('Height', body, html, computedStyle),
+    width: getSize('Width', body, html, computedStyle),
   };
 }
