816ca5965d76d0fdd5572a853031cb179186565a
Federico Zivolo <federico.zivolo@gmail.com>
2017-05-23 12:42:11 +0200

    [fix] Auto placement computation (#284)

--- src/popper/utils/computeAutoPlacement.js
+++ src/popper/utils/computeAutoPlacement.js
@@ -1,5 +1,9 @@
 import getBoundaries from '../utils/getBoundaries';
 
+function getArea({ width, height }) {
+  return width * height;
+}
+
 /**
  * Utility used to transform the `auto` placement to the placement with more
  * available space.
@@ -14,24 +18,56 @@ export default function computeAutoPlacement(
   refRect,
   popper,
   reference,
-  boundariesElement
+  boundariesElement,
+  padding = 0
 ) {
   if (placement.indexOf('auto') === -1) {
     return placement;
   }
 
-  const boundaries = getBoundaries(popper, reference, 0, boundariesElement);
+  const boundaries = getBoundaries(
+    popper,
+    reference,
+    padding,
+    boundariesElement
+  );
 
-  const sides = {
-    top: refRect.top - boundaries.top,
-    right: boundaries.right - refRect.right,
-    bottom: boundaries.bottom - refRect.bottom,
-    left: refRect.left - boundaries.left,
+  const rects = {
+    top: {
+      width: boundaries.width,
+      height: refRect.top - boundaries.top,
+    },
+    right: {
+      width: boundaries.right - refRect.right,
+      height: boundaries.height,
+    },
+    bottom: {
+      width: boundaries.width,
+      height: boundaries.bottom - refRect.bottom,
+    },
+    left: {
+      width: refRect.left - boundaries.left,
+      height: boundaries.height,
+    },
   };
 
-  const computedPlacement = Object.keys(sides).sort(
-    (a, b) => sides[b] - sides[a]
-  )[0];
+  const sortedAreas = Object.keys(rects)
+    .map(key => ({
+      key,
+      ...rects[key],
+      area: getArea(rects[key]),
+    }))
+    .sort((a, b) => b.area - a.area);
+
+  const filteredAreas = sortedAreas.filter(
+    ({ width, height }) =>
+      width >= popper.clientWidth && height >= popper.clientHeight
+  );
+
+  const computedPlacement = filteredAreas.length > 0
+    ? filteredAreas[0].key
+    : sortedAreas[0].key;
+
   const variation = placement.split('-')[1];
 
   return computedPlacement + (variation ? `-${variation}` : '');
