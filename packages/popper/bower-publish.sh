1323f1f8b621795760de2e3218743663e3c629e2
Federico Zivolo <fzivolo@quid.com>
2017-08-15 13:57:22 +0200

    chore: Fixed bower-publish script

--- packages/popper/bower-publish.sh
+++ packages/popper/bower-publish.sh
@@ -8,8 +8,6 @@ git add -f ../../dist/*
 git add ../../bower.json
 git commit --no-verify -m "chore(automatic): v${version} (dist files)"
 git tag -a v${version} -m "chore(automatic): v${version} (tag release)"
-rm -rf ../../dist
-rm -f ../../bower.json
 git rm ../../dist/**/*
 git rm ../../bower.json
 git commit --no-verify -m "chore(automatic): v${version} (dist files cleanup)"
