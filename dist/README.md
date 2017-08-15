4d792ab227b60ac34c96bdd6f40c145839020410
Federico Zivolo <fzivolo@quid.com>
2017-08-15 12:59:03 +0200

    v1.12.0

--- dist/README.md
+++
@@ -1,5 +0,0 @@
-# Looking for dist files?
-
-This repository is meant to host _exclusively_ the source code of the project.
-
-To find the pre-built library files please refer to the installation instructions in the [README](/README.md) file.
--- packages/popper/bower-publish.sh
+++ packages/popper/bower-publish.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-git add dist/**/*
-git tag -a v${version} -m "chore(automatic): v${version}"
-git rm dist/**/*
-git commit -m "chore(automatic): Clean dist after v${version}"
+version="$(cat package.json | jq -r '.version')"
+
+cp -R dist ../../dist
+cp bower.json ../../bower.json
+git add -f ../../dist/*
+git add ../../bower.json
+git commit --no-verify -m "chore(automatic): v${version} (dist files)"
+git tag -a v${version} -m "chore(automatic): v${version} (tag release)"
+rm -rf ../../dist
+rm -f ../../bower.json
+git rm ../../dist/**/*
+git rm ../../bower.json
+git commit --no-verify -m "chore(automatic): v${version} (dist files cleanup)"
