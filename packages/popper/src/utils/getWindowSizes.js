1b3a82d77a11ce84d767da0f1ac4982774a64979
Federico Zivolo <federico.zivolo@gmail.com>
2017-08-28 14:12:38 +0200

    fix(popper): Fixed regression #401 (#402)

--- packages/popper/src/utils/getWindowSizes.js
+++ packages/popper/src/utils/getWindowSizes.js
@@ -1,12 +1,12 @@
 import isIE10 from './isIE10';
 
-function getSize(axis, body, html, computedStyle) {
+function getSize(axis, body, html, computedStyle, includeScroll) {
   return Math.max(
     body[`offset${axis}`],
-    body[`scroll${axis}`],
+    includeScroll ? body[`scroll${axis}`] : 0,
     html[`client${axis}`],
     html[`offset${axis}`],
-    html[`scroll${axis}`],
+    includeScroll ? html[`scroll${axis}`] : 0,
     isIE10()
       ? html[`offset${axis}`] +
         computedStyle[`margin${axis === 'Height' ? 'Top' : 'Left'}`] +
@@ -15,13 +15,13 @@ function getSize(axis, body, html, computedStyle) {
   );
 }
 
-export default function getWindowSizes() {
+export default function getWindowSizes(includeScroll = true) {
   const body = window.document.body;
   const html = window.document.documentElement;
   const computedStyle = isIE10() && window.getComputedStyle(html);
 
   return {
-    height: getSize('Height', body, html, computedStyle),
-    width: getSize('Width', body, html, computedStyle),
+    height: getSize('Height', body, html, computedStyle, includeScroll),
+    width: getSize('Width', body, html, computedStyle, includeScroll),
   };
 }
