c6db75814be84b6b02a2d3058a386df0e71671c1
Kasper Isager <kasperisager@gmail.com>
2017-09-13 20:22:38 +0200

    Add missing annotation

--- packages/popper/src/utils/getWindow.js
+++ packages/popper/src/utils/getWindow.js
@@ -1,6 +1,7 @@
 /**
  * Get the window associated with the element
  * @argument {Element} element
+ * @returns {Window}
  */
 export default function getWindow(element) {
   const ownerDocument = element.ownerDocument;
