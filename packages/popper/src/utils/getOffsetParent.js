87d226c8b626ddae57b4aded72b1a9457d40c682
Roman Kushnir <roman.kushnir@ad2games.com>
2018-02-07 15:17:44 +0100

    Fix for IE10's special opinion on offsetParent

--- packages/popper/src/utils/getOffsetParent.js
+++ packages/popper/src/utils/getOffsetParent.js
@@ -1,4 +1,5 @@
 import getStyleComputedProperty from './getStyleComputedProperty';
+import isIE from './isIE';
 /**
  * Returns the offset parent of the given element
  * @method
@@ -7,12 +8,19 @@ import getStyleComputedProperty from './getStyleComputedProperty';
  * @returns {Element} offset parent
  */
 export default function getOffsetParent(element) {
+  if (!element) {
+    return document.documentElement;
+  }
+
+  const noOffsetParent = isIE(10) ? document.body : null;
+
   // NOTE: 1 DOM access here
-  let offsetParent = element && element.offsetParent;
+  let offsetParent = element.offsetParent;
   // Skip hidden elements which don't have an offsetParent
-  while (offsetParent === null && element.nextElementSibling) {
+  while (offsetParent === noOffsetParent && element.nextElementSibling) {
     offsetParent = (element = element.nextElementSibling).offsetParent;
   }
+
   const nodeName = offsetParent && offsetParent.nodeName;
 
   if (!nodeName || nodeName === 'BODY' || nodeName === 'HTML') {
