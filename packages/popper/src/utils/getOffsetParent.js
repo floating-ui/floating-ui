681e27e74519c1ec0c4a0aaa17d606bf75ac48e9
RKushnir <RKushnir@users.noreply.github.com>
2018-02-21 12:46:37 +0100

    fix: offset parent calculation for hidden elements (#545)
    * Fix offset parent calculation for hidden elements
    
    Fixes #537
    
    * Add an explanation for the offsetParent loop
    
    * Fix for IE10's special opinion on offsetParent
    
    * Make CodeClimate happy again

--- packages/popper/src/utils/getOffsetParent.js
+++ packages/popper/src/utils/getOffsetParent.js
@@ -1,4 +1,5 @@
 import getStyleComputedProperty from './getStyleComputedProperty';
+import isIE from './isIE';
 /**
  * Returns the offset parent of the given element
  * @method
@@ -7,16 +8,23 @@ import getStyleComputedProperty from './getStyleComputedProperty';
  * @returns {Element} offset parent
  */
 export default function getOffsetParent(element) {
+  if (!element) {
+    return document.documentElement;
+  }
+
+  const noOffsetParent = isIE(10) ? document.body : null;
+
   // NOTE: 1 DOM access here
-  const offsetParent = element && element.offsetParent;
+  let offsetParent = element.offsetParent;
+  // Skip hidden elements which don't have an offsetParent
+  while (offsetParent === noOffsetParent && element.nextElementSibling) {
+    offsetParent = (element = element.nextElementSibling).offsetParent;
+  }
+
   const nodeName = offsetParent && offsetParent.nodeName;
 
   if (!nodeName || nodeName === 'BODY' || nodeName === 'HTML') {
-    if (element) {
-      return element.ownerDocument.documentElement
-    }
-
-    return document.documentElement;
+    return element ? element.ownerDocument.documentElement : document.documentElement;
   }
 
   // .offsetParent will return the closest TD or TABLE in case
