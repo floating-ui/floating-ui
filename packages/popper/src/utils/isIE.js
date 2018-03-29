df071e6342e445a728de1bffa61ba0e9cb53bdcd
atomiks <cc.glows@gmail.com>
2018-03-30 00:50:23 +1000

    Make isIE minifier friendly

--- packages/popper/src/utils/isIE.js
+++ packages/popper/src/utils/isIE.js
@@ -1,30 +1,19 @@
 /**
- * Tells if you are running Internet Explorer
+ * Determines if the browser is Internet Explorer
  * @method
  * @memberof Popper.Utils
- * @argument {number} version to check
+ * @param {number} version to check
  * @returns {Boolean} isIE
  */
-const cache = {};
+export default (() => {
+  const ua = navigator.userAgent;
+  const isIE11 = /Trident/.test(ua);
+  const isIE10 = /MISE 10/.test(ua);
 
-export default function (version = 'all') {
-  version = version.toString();
-  if(cache.hasOwnProperty(version)){
-    return cache[version];
-  }
-  switch (version) {
-    case '11':
-      cache[version] = navigator.userAgent.indexOf('Trident') !== -1;
-      break;
-    case '10':
-      cache[version] = navigator.appVersion.indexOf('MSIE 10') !== -1;
-      break;
-    case 'all':
-      cache[version] = navigator.userAgent.indexOf('Trident') !== -1 || navigator.userAgent.indexOf('MSIE') !== -1;
-      break;
-  }
-
-  //Set IE
-  cache.all = cache.all || Object.keys(cache).some(key => cache[key]);
-  return cache[version];
-}
+  return (version = 'all') => {
+    if (version === 'all') return isIE11 || isIE10;
+    if (version === 11) return isIE11;
+    if (version === 10) return isIE10;
+    return false;
+  };
+})();
