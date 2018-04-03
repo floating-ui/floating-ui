aca2babd1ee04306404e8c116c6b7ba8ee4221eb
atomiks <cc.glows@gmail.com>
2018-04-03 21:01:39 +1000

    Patch pull #594

--- packages/popper/src/utils/isIE.js
+++ packages/popper/src/utils/isIE.js
@@ -1,30 +1,22 @@
+import isBrowser from './isBrowser'
+
+const ua = isBrowser && navigator.userAgent;
+const isIE11 = /Trident/.test(ua);
+const isIE10 = /MSIE 10/.test(ua);
+
 /**
- * Tells if you are running Internet Explorer
+ * Determines if the browser is Internet Explorer
  * @method
  * @memberof Popper.Utils
- * @argument {number} version to check
+ * @param {Number} version to check
  * @returns {Boolean} isIE
  */
-const cache = {};
-
-export default function (version = 'all') {
-  version = version.toString();
-  if(cache.hasOwnProperty(version)){
-    return cache[version];
+export default function isIE(version) {
+  if (version === 11) {
+    return isIE11;
   }
-  switch (version) {
-    case '11':
-      cache[version] = navigator.userAgent.indexOf('Trident') !== -1;
-      break;
-    case '10':
-      cache[version] = navigator.appVersion.indexOf('MSIE 10') !== -1;
-      break;
-    case 'all':
-      cache[version] = navigator.userAgent.indexOf('Trident') !== -1 || navigator.userAgent.indexOf('MSIE') !== -1;
-      break;
+  if (version === 10) {
+    return isIE10;
   }
-
-  //Set IE
-  cache.all = cache.all || Object.keys(cache).some(key => cache[key]);
-  return cache[version];
+  return isIE11 || isIE10;
 }
