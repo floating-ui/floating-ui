floating-ui/floating-ui

commit 7bfdedff75d694caef7ce30e3ab8c89df1b7f0bb

fix: Fixed #251 (finally) (#596)

packages/popper/src/methods/update.js
@@ -2,6 +2,7 @@ import computeAutoPlacement from '../utils/computeAutoPlacement';
 import getReferenceOffsets from '../utils/getReferenceOffsets';
 import getPopperOffsets from '../utils/getPopperOffsets';
 import runModifiers from '../utils/runModifiers';
+import getSupportedPropertyName from '../utils/getSupportedPropertyName';
 
 /**
  * Updates the position of the popper, computing the new offsets and applying
@@ -24,6 +25,14 @@ export default function update() {
     flipped: false,
     offsets: {},
   };
+
+  // NOTE: DOM access here
+  // resets the popper's position so that the document size can be calculated excluding
+  // the size of the popper element itself
+  const popperStyles = this.popper.style;
+  popperStyles.top = '';
+  popperStyles.left = '';
+  popperStyles[getSupportedPropertyName('transform')] = '';
 
   // compute reference element offsets
   data.offsets.reference = getReferenceOffsets(

commit 2aaade11a70e76526bf7522042b2f68d138e8225

fix: Regression introduced by #596
More details here: https://github.com/souporserious/react-popper/issues/122

packages/popper/src/methods/update.js
@@ -26,14 +26,6 @@ export default function update() {
     offsets: {},
   };
 
-  // NOTE: DOM access here
-  // resets the popper's position so that the document size can be calculated excluding
-  // the size of the popper element itself
-  const popperStyles = this.popper.style;
-  popperStyles.top = '';
-  popperStyles.left = '';
-  popperStyles[getSupportedPropertyName('transform')] = '';
-
   // compute reference element offsets
   data.offsets.reference = getReferenceOffsets(
     this.state,
@@ -59,12 +51,31 @@ export default function update() {
 
   data.positionFixed = this.options.positionFixed;
 
+
+  // NOTE: DOM access here
+  // resets the popper's position so that the document size can be calculated excluding
+  // the size of the popper element itself
+  const transformProp = getSupportedPropertyName('transform');
+  const popperStyles = this.popper.style; // assignment to help minification
+  const { top, left, [transformProp]: transform } = popperStyles;
+  popperStyles.top = '';
+  popperStyles.left = '';
+  popperStyles[transformProp] = '';
+
+
   // compute the popper offsets
   data.offsets.popper = getPopperOffsets(
     this.popper,
     data.offsets.reference,
     data.placement
   );
+
+  // NOTE: DOM access here
+  // restores the original style properties after the offsets have been computed
+  popperStyles.top = top;
+  popperStyles.left = left;
+  popperStyles[transformProp] = transform;
+
   data.offsets.popper.position = this.options.positionFixed ? 'fixed' : 'absolute';
 
   // run the modifiers