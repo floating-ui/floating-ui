4e0ad960cd8bece5e742e480e173f2802a75c859
atomiks <cc.glows@gmail.com>
2019-11-21 19:16:36 +1100

    fix(detectOverflow): remove boundary margin calc (#840)
    * fix: remove boundary margin calc
    
    * refactor: remove redundant object
    
    * fix: revert object spread operator

--- src/modifiers/detectOverflow.js
+++ src/modifiers/detectOverflow.js
@@ -1,8 +1,6 @@
 // @flow
 import type { State, Modifier } from '../types';
 import getBoundingClientRect from '../dom-utils/getBoundingClientRect';
-import getElementClientRect from '../dom-utils/getElementClientRect';
-import getElementMargins from '../dom-utils/getElementMargins';
 import computeOffsets from '../utils/computeOffsets';
 
 type Options = {
@@ -25,7 +23,7 @@ export function detectOverflow(
   const popperElement = state.elements.popper;
   const referenceElement = state.elements.reference;
   const popperRect = state.measures.popper;
-  const referenceRect = state.measures.reference;
+
   if (!options.boundaryElement.contains(popperElement)) {
     console.error(
       'PopperJS: "detectOverflow" can accept as `boundaryElement` only a parent node of the provided popper.'
@@ -35,14 +33,6 @@ export function detectOverflow(
 
   const boundaryClientRect = getBoundingClientRect(options.boundaryElement);
   const referenceClientRect = getBoundingClientRect(referenceElement);
-  const boundaryMargins = getElementMargins(options.boundaryElement);
-  const boundaryRect = {
-    left: boundaryClientRect.left - boundaryMargins.left,
-    right: boundaryClientRect.right - boundaryMargins.right,
-    top: boundaryClientRect.top - boundaryMargins.top,
-    bottom:
-      boundaryClientRect.bottom - boundaryMargins.top - boundaryMargins.bottom,
-  };
 
   const popperOffsets = computeOffsets({
     reference: referenceClientRect,
@@ -55,8 +45,6 @@ export function detectOverflow(
     },
   });
 
-  console.log(boundaryMargins);
-
   const popperClientRect = Object.assign({}, popperOffsets, {
     top: popperOffsets.y,
     bottom: popperOffsets.y + popperRect.height,
@@ -65,13 +53,13 @@ export function detectOverflow(
   });
 
   state.modifiersData.detectOverflow = {
-    top: boundaryRect.top > popperClientRect.top,
-    bottom: boundaryRect.bottom < popperClientRect.bottom,
-    left: boundaryRect.left > popperClientRect.left,
-    right: boundaryRect.right < popperClientRect.right,
+    top: boundaryClientRect.top > popperClientRect.top,
+    bottom: boundaryClientRect.bottom < popperClientRect.bottom,
+    left: boundaryClientRect.left > popperClientRect.left,
+    right: boundaryClientRect.right < popperClientRect.right,
   };
 
-  console.log(boundaryRect, popperClientRect);
+  console.log(boundaryClientRect, popperClientRect);
   console.log(state.modifiersData.detectOverflow);
 
   return state;
