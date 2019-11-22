7864cb1ba20bf37e7261e73ba7720afde7f35d6e
Federico Zivolo <federico.zivolo@gmail.com>
2019-11-22 10:39:39 +0100

    feat: add support for placement variations

--- src/utils/computeOffsets.js
+++ src/utils/computeOffsets.js
@@ -1,5 +1,7 @@
 // @flow
 import getBasePlacement from './getBasePlacement';
+import getVariationPlacement from './getVariationPlacement';
+import getMainAxisFromPlacement from './getMainAxisFromPlacement';
 import getAltAxis from './getAltAxis';
 import getAltLen from './getAltLen';
 import type {
@@ -8,7 +10,7 @@ import type {
   Offsets,
   ClientRectObject,
 } from '../types';
-import { top, right, bottom, left, type Placement } from '../enums';
+import { top, right, bottom, left, start, end, type Placement } from '../enums';
 
 export default ({
   reference,
@@ -24,34 +26,61 @@ export default ({
   scroll: { scrollTop: number, scrollLeft: number },
 }): Offsets => {
   const basePlacement = placement ? getBasePlacement(placement) : null;
+  const variationPlacement = placement
+    ? getVariationPlacement(placement)
+    : null;
 
   const { scrollTop, scrollLeft } = scroll;
 
+  let offsets;
   switch (basePlacement) {
     case top:
-      return {
+      offsets = {
         x: reference.x + reference.width / 2 - element.width / 2 - scrollLeft,
         y: reference.y - element.height - scrollTop,
       };
+      break;
     case bottom:
-      return {
+      offsets = {
         x: reference.x + reference.width / 2 - element.width / 2 - scrollLeft,
         y: reference.y + reference.height - scrollTop,
       };
+      break;
     case right:
-      return {
+      offsets = {
         x: reference.x + reference.width - scrollLeft,
         y: reference.y + reference.height / 2 - element.height / 2 - scrollTop,
       };
+      break;
     case left:
-      return {
+      offsets = {
         x: reference.x - element.width - scrollLeft,
         y: reference.y + reference.height / 2 - element.height / 2 - scrollTop,
       };
+      break;
     default:
-      return {
+      offsets = {
         x: reference.x - scrollLeft,
         y: reference.y - scrollTop,
       };
   }
+
+  const mainAxis = placement ? getMainAxisFromPlacement(placement) : null;
+  const altAxis = mainAxis ? getAltAxis(mainAxis) : null;
+  const len = altAxis === 'x' ? 'width' : 'height';
+  const axis = [right, left].includes(basePlacement) ? mainAxis : altAxis;
+
+  if (axis != null) {
+    switch (variationPlacement) {
+      case start:
+        offsets[axis] -= element[len] / 2;
+        break;
+      case end:
+        offsets[axis] += element[len] / 2;
+        break;
+      default:
+    }
+  }
+
+  return offsets;
 };
