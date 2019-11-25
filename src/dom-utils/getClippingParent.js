2889ad86d8209998a14746119c24b86069942398
Federico Zivolo <federico.zivolo@gmail.com>
2019-11-25 11:33:55 +0100

    fix: preserve offsetParent on getClippingParent traversing

--- src/dom-utils/getClippingParent.js
+++ src/dom-utils/getClippingParent.js
@@ -6,14 +6,20 @@ import getWindow from './getWindow';
 
 // A "clipping parent" is a scrolling container with the characteristic of
 // clipping (or hiding) overflowing elements with a position different from `initial`
-export default function getClippingParent(element: HTMLElement): HTMLElement {
+export default function getClippingParent(
+  element: HTMLElement,
+  offsetElement?: Element
+): HTMLElement {
   const scrollParent = getScrollParent(element);
-  const offsetParent = getOffsetParent(element);
+  const offsetParent = offsetElement || getOffsetParent(element);
   const win = getWindow(element);
 
   return offsetParent === win
     ? element.ownerDocument.documentElement
     : scrollParent.contains(offsetParent)
     ? scrollParent
-    : getClippingParent(getScrollParent(getParentNode(scrollParent)));
+    : getClippingParent(
+        getScrollParent(getParentNode(scrollParent)),
+        offsetParent
+      );
 }
