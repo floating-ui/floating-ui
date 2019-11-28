6055151b88734de9f3dec4bf0c2f6487caf602b1
Federico Zivolo <federico.zivolo@gmail.com>
2019-11-28 16:59:30 +0100

    Revert "test: fix default browser value"
    This reverts commit a8f88984443a15616c9bdfba67ae7e42dd179ddd.

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,12 +1,11 @@
-module.exports = () =>
-  ({
-    browser: process.env.BROWSER || 'chromium',
-    launch: {
-      dumpio: true,
-      headless: process.env.HEADLESS !== 'false',
-    },
-    server: {
-      command: 'yarn dev:serve',
-      port: 5000,
-    },
-  }());
+module.exports = {
+  browser: process.env.BROWSER || 'chromium',
+  launch: {
+    dumpio: true,
+    headless: process.env.HEADLESS !== 'false',
+  },
+  server: {
+    command: 'yarn dev:serve',
+    port: 5000,
+  },
+};
