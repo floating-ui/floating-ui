a8f88984443a15616c9bdfba67ae7e42dd179ddd
Federico Zivolo <federico.zivolo@gmail.com>
2019-11-28 16:54:44 +0100

    test: fix default browser value

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,11 +1,12 @@
-module.exports = {
-  browser: process.env.BROWSER || 'chromium',
-  launch: {
-    dumpio: true,
-    headless: process.env.HEADLESS !== 'false',
-  },
-  server: {
-    command: 'yarn dev:serve',
-    port: 5000,
-  },
-};
+module.exports = () =>
+  ({
+    browser: process.env.BROWSER || 'chromium',
+    launch: {
+      dumpio: true,
+      headless: process.env.HEADLESS !== 'false',
+    },
+    server: {
+      command: 'yarn dev:serve',
+      port: 5000,
+    },
+  }());
