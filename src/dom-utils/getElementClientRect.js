34c3cd5d7c64d74c6f3a0be8ad0be8c7ebe31684
Carbon Copy <cc.glows@gmail.com>
2019-11-30 00:48:03 +1100

    fix: handle offsetParent window scroll

--- src/dom-utils/getElementClientRect.js
+++ src/dom-utils/getElementClientRect.js
@@ -8,22 +8,28 @@ import getWindowScroll from './getWindowScroll';
 
 // Returns the width, height and offsets of the provided element relative to the
 // offsetParent
+// TODO: take into account nested offsetParents
 export default (element: Element): Rect => {
   const rect = getBoundingClientRect(element);
-  const offsetParent = getOffsetParent(element);
   const scrollParentsScroll = getScrollSum(listScrollParents(element));
   const windowScroll = getWindowScroll(element);
-  const scrollLeft = scrollParentsScroll.scrollLeft - windowScroll.scrollLeft;
-  const scrollTop = scrollParentsScroll.scrollTop - windowScroll.scrollTop;
-  const { left, top } =
-    offsetParent instanceof Element
-      ? getBoundingClientRect(offsetParent)
-      : { left: 0, top: 0 };
+  const offsetParent = getOffsetParent(element);
+  const isOffsetParentElement = offsetParent instanceof Element;
+  const offsetParentRect = isOffsetParentElement
+    ? getBoundingClientRect(offsetParent)
+    : { left: 0, top: 0 };
+
+  const scrollLeft =
+    scrollParentsScroll.scrollLeft -
+    (isOffsetParentElement ? windowScroll.scrollLeft : 0);
+  const scrollTop =
+    scrollParentsScroll.scrollTop -
+    (isOffsetParentElement ? windowScroll.scrollTop : 0);
 
   const width = rect.width;
   const height = rect.height;
-  const x = rect.left + scrollLeft - left;
-  const y = rect.top + scrollTop - top;
+  const x = rect.left + scrollLeft - offsetParentRect.left;
+  const y = rect.top + scrollTop - offsetParentRect.top;
 
   return { width, height, x, y };
 };
