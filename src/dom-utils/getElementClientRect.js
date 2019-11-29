634e0e9aa8ead6aa657407297f8b701ced45b830
Carbon Copy <cc.glows@gmail.com>
2019-11-30 01:34:17 +1100

    fix: handle nested offsetParents

--- src/dom-utils/getElementClientRect.js
+++ src/dom-utils/getElementClientRect.js
@@ -4,32 +4,43 @@ import getBoundingClientRect from './getBoundingClientRect';
 import listScrollParents from './listScrollParents';
 import getScrollSum from './getScrollSum';
 import getOffsetParent from './getOffsetParent';
-import getWindowScroll from './getWindowScroll';
 
 // Returns the width, height and offsets of the provided element relative to the
 // offsetParent
-// TODO: take into account nested offsetParents
 export default (element: Element): Rect => {
   const rect = getBoundingClientRect(element);
   const scrollParentsScroll = getScrollSum(listScrollParents(element));
-  const windowScroll = getWindowScroll(element);
-  const offsetParent = getOffsetParent(element);
-  const isOffsetParentElement = offsetParent instanceof Element;
-  const offsetParentRect = isOffsetParentElement
-    ? getBoundingClientRect(offsetParent)
+  const directOffsetParent = getOffsetParent(element);
+  const isOffsetParentElement = directOffsetParent instanceof Element;
+  const directOffsetParentRect = isOffsetParentElement
+    ? getBoundingClientRect(directOffsetParent)
     : { left: 0, top: 0 };
 
-  const scrollLeft =
-    scrollParentsScroll.scrollLeft -
-    (isOffsetParentElement ? windowScroll.scrollLeft : 0);
-  const scrollTop =
-    scrollParentsScroll.scrollTop -
-    (isOffsetParentElement ? windowScroll.scrollTop : 0);
+  const scrollLeft = scrollParentsScroll.scrollLeft;
+  const scrollTop = scrollParentsScroll.scrollTop;
+
+  const ancestorOffsetParents = [];
+  let currentOffsetParent = directOffsetParent;
+
+  while (currentOffsetParent instanceof Element) {
+    currentOffsetParent = getOffsetParent(currentOffsetParent);
+    ancestorOffsetParents.push(currentOffsetParent);
+  }
+
+  const ancestorOffsetParentScrollSum = getScrollSum(ancestorOffsetParents);
 
   const width = rect.width;
   const height = rect.height;
-  const x = rect.left + scrollLeft - offsetParentRect.left;
-  const y = rect.top + scrollTop - offsetParentRect.top;
+  const x =
+    rect.left +
+    scrollLeft -
+    directOffsetParentRect.left -
+    ancestorOffsetParentScrollSum.scrollLeft;
+  const y =
+    rect.top +
+    scrollTop -
+    directOffsetParentRect.top -
+    ancestorOffsetParentScrollSum.scrollTop;
 
   return { width, height, x, y };
 };
