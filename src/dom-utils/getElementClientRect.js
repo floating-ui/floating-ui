9370d0ce6915eb2643af4fcc488996401aae57f2
Carbon Copy <cc.glows@gmail.com>
2019-11-29 21:10:45 +1100

    fix: margin/padding on body/html

--- src/dom-utils/getElementClientRect.js
+++ src/dom-utils/getElementClientRect.js
@@ -3,17 +3,27 @@ import type { Rect } from '../types';
 import getBoundingClientRect from './getBoundingClientRect';
 import listScrollParents from './listScrollParents';
 import getScrollSum from './getScrollSum';
+import getOffsetParent from './getOffsetParent';
+import getWindowScroll from './getWindowScroll';
 
 // Returns the width, height and offsets of the provided element relative to the
 // offsetParent
 export default (element: HTMLElement): Rect => {
   const rect = getBoundingClientRect(element);
-  const { scrollLeft, scrollTop } = getScrollSum(listScrollParents(element));
+  const offsetParent = getOffsetParent(element);
+  const scrollParentsScroll = getScrollSum(listScrollParents(element));
+  const windowScroll = getWindowScroll(element);
+  const scrollLeft = scrollParentsScroll.scrollLeft - windowScroll.scrollLeft;
+  const scrollTop = scrollParentsScroll.scrollTop - windowScroll.scrollTop;
+  const { left, top } =
+    offsetParent instanceof Element
+      ? getBoundingClientRect(offsetParent)
+      : { left: 0, top: 0 };
 
   const width = rect.width;
   const height = rect.height;
-  const x = rect.left + scrollLeft;
-  const y = rect.top + scrollTop;
+  const x = rect.left + scrollLeft - left;
+  const y = rect.top + scrollTop - top;
 
   return { width, height, x, y };
 };
