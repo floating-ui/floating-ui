76e6ac14ffbb13f83a2606d1bd2fba00d5e2d0de
Federico Zivolo <federico.zivolo@gmail.com>
2019-11-30 16:33:56 +0100

    test: arrow modifier functional tests

---
+++ tests/functional/arrow.test.js
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment jest-environment-puppeteer
+ * @flow
+ */
+import { scroll, screenshot } from '../utils/puppeteer.js';
+
+it('shuld center the arrow', async () => {
+  const page = await browser.newPage();
+  await page.goto('http://localhost:5000/modifiers/arrow/index.html');
+
+  await scroll(page, '.scroll3', 300);
+  await scroll(page, '.scroll2', 200);
+  await scroll(page, '.scroll1', 200);
+
+  expect(await screenshot(page)).toMatchImageSnapshot();
+});
+
+it('should keep arrow within popper edges', async () => {
+  const page = await browser.newPage();
+  await page.goto('http://localhost:5000/modifiers/arrow/index.html');
+
+  await scroll(page, '.scroll2', 400);
+  await scroll(page, '.scroll3', 200);
+  await scroll(page, '.scroll1', 450);
+
+  expect(await screenshot(page)).toMatchImageSnapshot();
+});
