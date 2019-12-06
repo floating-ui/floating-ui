4a1c5fdcbf16510a17e85a97520997914ed943f5
Federico Zivolo <federico.zivolo@gmail.com>
2019-12-06 13:51:16 +0100

    chore: refactor build process

--- rollup.config.js
+++ rollup.config.js
@@ -8,87 +8,44 @@ import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 import flowEntry from 'rollup-plugin-flow-entry';
 import pkg from './package.json';
 
-const IS_DEV = process.env.NODE_ENV === 'development';
-const dir = IS_DEV ? 'tests/visual/dist' : 'dist';
-
-const inputs = ['src/popper.js', 'src/popper-lite.js', 'src/popper-base.js'];
-
 const getFileName = input => input.split('/')[1].split('.')[0];
 
-const banner = license({ banner: `@popperjs/core v${pkg.version}` });
+const inputs = ['src/popper.js', 'src/popper-lite.js', 'src/popper-base.js'];
+const bundles = [
+  { inputs, format: 'umd', dir: 'dist', minify: true, flow: true },
+  { inputs, format: 'cjs', dir: 'dist' },
+  { inputs, format: 'esm', dir: 'dist' },
+];
 
-const createUmdBundle = ({ input, minify } = {}) => ({
-  input,
-  plugins: [
-    replace({
-      __DEV__: minify ? 'false' : 'true',
-    }),
-    babel(),
-    sizeSnapshot({ printInfo: false }),
-    minify && terser(),
-    banner,
-    flowEntry(),
-    bundleSize(),
-    visualizer({
-      sourcemap: true,
-      filename: `stats/${getFileName(input)}${minify ? '-min' : ''}.html`,
-    }),
-  ].filter(Boolean),
-  output: {
-    name: 'Popper',
-    file: `${dir}/umd/${getFileName(input)}${minify ? '.min' : ''}.js`,
-    format: 'umd',
-    sourcemap: true,
-  },
-});
+const configs = bundles
+  .map(({ inputs, dir, format, minify, flow }) =>
+    inputs.map(input => ({
+      input,
+      plugins: [
+        replace({
+          __DEV__: minify ? 'false' : 'true',
+        }),
+        babel(),
+        sizeSnapshot({ printInfo: false }),
+        minify && terser(),
+        license({ banner: `@popperjs/core v${pkg.version}` }),
+        flow && flowEntry(),
+        bundleSize(),
+        visualizer({
+          sourcemap: true,
+          filename: `stats/${getFileName(input)}${minify ? '-min' : ''}.html`,
+        }),
+      ].filter(Boolean),
+      output: {
+        name: 'Popper',
+        file: `${dir}/${format}/${getFileName(input)}${
+          minify ? '.min' : ''
+        }.js`,
+        format,
+        sourcemap: true,
+      },
+    }))
+  )
+  .flat();
 
-const createEsmBundle = ({ input }) => ({
-  input: 'src/index.js',
-  plugins: [babel(), banner, bundleSize()],
-  output: {
-    name: 'Popper',
-    file: `${dir}/esm/${getFileName(input)}.js`,
-    format: 'esm',
-    sourcemap: true,
-  },
-});
-
-const createCjsBundle = ({ input }) => ({
-  input,
-  plugins: [babel(), banner, bundleSize()],
-  output: {
-    file: `${dir}/cjs/${getFileName(input)}.js`,
-    format: 'cjs',
-    sourcemap: true,
-  },
-});
-
-const createDevBundle = ({ input }) => ({
-  input,
-  plugins: [
-    babel(),
-    banner,
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('development'),
-    }),
-  ],
-  output: {
-    name: 'Popper',
-    file: `${dir}/esm/${getFileName(input)}.js`,
-    format: 'esm',
-    sourcemap: true,
-  },
-});
-
-const builds = IS_DEV
-  ? [createDevBundle({ input: 'src/popper.js' })]
-  : inputs
-      .map(input => [
-        createUmdBundle({ input }),
-        createUmdBundle({ input, minify: true }),
-        createEsmBundle({ input }),
-        createCjsBundle({ input }),
-      ])
-      .flat();
-
-export default builds;
+export default configs;
