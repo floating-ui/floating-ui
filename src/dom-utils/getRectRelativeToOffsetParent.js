03f6fe6958755568208b65e338080097514745cd
atomiks <cc.glows@gmail.com>
2019-12-07 21:46:23 +1100

    fix: positioning logic for offsetParents

--- src/dom-utils/getRectRelativeToOffsetParent.js
+++ src/dom-utils/getRectRelativeToOffsetParent.js
@@ -15,37 +15,43 @@ export default (
 ): Rect => {
   const unwrappedElement = unwrapVirtualElement(element);
   const rect = getBoundingClientRect(element);
-  const scrollParentsScrollSum = getScrollSum(
-    listScrollParents(unwrappedElement)
-  );
-  const directOffsetParent = getOffsetParent(unwrappedElement);
-  const directOffsetParentRect =
-    isElement(directOffsetParent) && !isFixed
-      ? getBoundingClientRect(directOffsetParent)
+  const scrollParents = listScrollParents(unwrappedElement);
+  const offsetParent = getOffsetParent(unwrappedElement);
+  const offsetParentRect =
+    isElement(offsetParent) && !isFixed
+      ? getBoundingClientRect(offsetParent)
       : { left: 0, top: 0 };
 
-  const ancestorOffsetParents = isFixed ? [directOffsetParent] : [];
-  let currentOffsetParent = directOffsetParent;
+  const relevantScrollParents = [];
 
-  while (isElement(currentOffsetParent)) {
-    currentOffsetParent = getOffsetParent(currentOffsetParent);
-    ancestorOffsetParents.push(currentOffsetParent);
+  // We want all the scrolling containers only up to and including the
+  // offsetParent
+  for (let i = 0; i < scrollParents.length; i++) {
+    const scrollParent = scrollParents[i];
+    relevantScrollParents.push(scrollParent);
+
+    if (scrollParent === offsetParent) {
+      break;
+    }
   }
 
-  const ancestorOffsetParentScrollSum = getScrollSum(ancestorOffsetParents);
+  const scrollSum = getScrollSum(relevantScrollParents);
+  const offsetParentScrollSum = isFixed
+    ? getScrollSum([offsetParent])
+    : { scrollLeft: 0, scrollTop: 0 };
 
   const width = rect.width;
   const height = rect.height;
   const x =
     rect.left +
-    scrollParentsScrollSum.scrollLeft -
-    ancestorOffsetParentScrollSum.scrollLeft -
-    directOffsetParentRect.left;
+    scrollSum.scrollLeft -
+    offsetParentScrollSum.scrollLeft -
+    offsetParentRect.left;
   const y =
     rect.top +
-    scrollParentsScrollSum.scrollTop -
-    ancestorOffsetParentScrollSum.scrollTop -
-    directOffsetParentRect.top;
+    scrollSum.scrollTop -
+    offsetParentScrollSum.scrollTop -
+    offsetParentRect.top;
 
   return { width, height, x, y };
 };
