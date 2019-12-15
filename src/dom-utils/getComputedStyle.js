06ada69705581122498b8cf2d804a6eec5f4ba98
Federico Zivolo <federico.zivolo@gmail.com>
2019-12-15 16:52:35 +0100

    fix: Flow improvements

--- src/dom-utils/getComputedStyle.js
+++ src/dom-utils/getComputedStyle.js
@@ -1,6 +1,8 @@
 // @flow
 import getWindow from './getWindow';
 
-export default function getComputedStyle(element: Element) {
+export default function getComputedStyle(
+  element: Element
+): CSSStyleDeclaration {
   return getWindow(element).getComputedStyle(element);
 }
