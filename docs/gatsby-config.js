25b5c7a02d6033a9d889ed7ca01bb8db913579d2
Federico Zivolo <federico.zivolo@gmail.com>
2020-01-13 15:04:21 +0100

    docs: redirect old docs page to new one

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -99,6 +99,13 @@ module.exports = {
         icon: `src/images/popper-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-redirect-from',
+      options: {
+        query: 'allMdx',
+      },
+    },
+    'gatsby-plugin-meta-redirect',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
