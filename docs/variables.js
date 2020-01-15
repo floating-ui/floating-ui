34d4a1a6105f7f3007a8aaa569b2f6eddb94f902
Federico Zivolo <federico.zivolo@gmail.com>
2020-01-15 13:44:59 +0100

    docs: read version from package.json

--- docs/variables.js
+++ docs/variables.js
@@ -1,3 +1,4 @@
 /* eslint-disable no-unused-vars */
-const VERSION_RANGE = (module.exports.VERSION_RANGE = '2.0.0-rc.1');
+const { version } = require('../package.json');
+const VERSION_RANGE = (module.exports.VERSION_RANGE = version);
 const UNPKG_CDN_URL = (module.exports.UNPKG_CDN_URL = `https://unpkg.com/@popperjs/core@${VERSION_RANGE}`);
