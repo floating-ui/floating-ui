7670a0e9680325ec6b64d729f159e97b40370825
Federico Zivolo <federico.zivolo@gmail.com>
2020-01-22 12:36:11 +0100

    perf: make contains utility faster

--- src/dom-utils/contains.js
+++ src/dom-utils/contains.js
@@ -1,16 +1,24 @@
 // @flow
 export default function contains(parent: Element, child: Element) {
+  const isShadow =
+    child.getRootNode && child.getRootNode().hasOwnProperty('host');
+
+  // First, attempt with faster native method
   if (parent.contains(child)) {
     return true;
   }
+  // then fallback to custom implementation with Shadow DOM support
+  else if (isShadow) {
+    let next = child;
+    do {
+      if (next && next.isSameNode(parent)) {
+        return true;
+      }
+      // $FlowFixMe: need a better way to handle this...
+      next = next.parentNode || next.host;
+    } while (next);
+  }
 
-  let next = child;
-  do {
-    if (next && next.isSameNode(parent)) {
-      return true;
-    }
-    // $FlowFixMe: need a better way to handle this...
-    next = next.parentNode || next.host;
-  } while (next);
+  // Give up, the result is false
   return false;
 }
