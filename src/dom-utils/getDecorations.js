8e3f2d8732882bc287bfa964afc6047fad1477d2
atomiks <cc.glows@gmail.com>
2020-01-25 10:55:19 +1100

    fix: take into account document element scrollbar

--- src/dom-utils/getDecorations.js
+++ src/dom-utils/getDecorations.js
@@ -1,15 +1,26 @@
 // @flow
 import type { SideObject } from '../types';
 import getBorders from './getBorders';
+import getNodeName from './getNodeName';
+import getWindow from './getWindow';
 
 // Borders + scrollbars
 export default function getDecorations(element: HTMLElement): SideObject {
   const borders = getBorders(element);
+  const win = getWindow(element);
+
+  let right = element.offsetWidth - element.clientWidth - borders.right;
+  let bottom = element.offsetHeight - element.clientHeight - borders.bottom;
+
+  if (getNodeName(element) === 'html') {
+    right = win.innerWidth - element.clientWidth;
+    bottom = win.innerHeight - element.clientHeight;
+  }
 
   return {
     top: borders.top,
-    right: element.offsetWidth - (element.clientWidth + borders.right),
-    bottom: element.offsetHeight - (element.clientHeight + borders.bottom),
+    right,
+    bottom,
     left: borders.left,
   };
 }
