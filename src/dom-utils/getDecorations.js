c878495bc382a51ec66ebeb5ba56a92251ff05de
Federico Zivolo <federico.zivolo@gmail.com>
2020-01-27 18:42:35 +0100

    fix: rtl support (fix #976)

--- src/dom-utils/getDecorations.js
+++ src/dom-utils/getDecorations.js
@@ -3,6 +3,7 @@ import type { SideObject } from '../types';
 import getBorders from './getBorders';
 import getNodeName from './getNodeName';
 import getWindow from './getWindow';
+import getComputedStyle from './getComputedStyle';
 
 // Borders + scrollbars
 export default function getDecorations(element: HTMLElement): SideObject {
@@ -12,7 +13,10 @@ export default function getDecorations(element: HTMLElement): SideObject {
   let right = element.offsetWidth - element.clientWidth - borders.right;
   let bottom = element.offsetHeight - element.clientHeight - borders.bottom;
 
-  if (getNodeName(element) === 'html') {
+  if (
+    getNodeName(element) === 'html' &&
+    getComputedStyle(element).direction !== 'rtl'
+  ) {
     right = win.innerWidth - element.clientWidth;
     bottom = win.innerHeight - element.clientHeight;
   }
