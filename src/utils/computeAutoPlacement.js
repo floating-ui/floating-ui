11868a9fbc6ce0cec1c1a166e6eaacc2849e063a
Federico Zivolo <federico.zivolo@gmail.com>
2020-01-28 08:40:22 +0100

    fix: remove String.prototype.includes

--- src/utils/computeAutoPlacement.js
+++ src/utils/computeAutoPlacement.js
@@ -42,7 +42,9 @@ export default function computeAutoPlacement(
   const placements = variation
     ? flipVariations
       ? variationPlacements
-      : variationPlacements.filter(placement => placement.includes(variation))
+      : variationPlacements.filter(
+          placement => placement.indexOf(variation) >= 0
+        )
     : basePlacements;
 
   // $FlowFixMe: Flow seems to have problems with two array unions...
