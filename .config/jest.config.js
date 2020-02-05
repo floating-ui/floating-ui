6c9ed15ac4c74863ebbaaf34281000aef1a26bca
maksim.markelov <Maksim.Markelov@nexign.com>
2020-02-05 10:38:52 +0300

    Replace globalSetup and globalTeardown with preset

--- .config/jest.config.js
+++ .config/jest.config.js
@@ -1,11 +1,10 @@
 module.exports = {
+  preset: 'jest-playwright-preset',
   testMatch: ['<rootDir>/src/**/*.test.js', '<rootDir>/tests/**/*.test.js'],
   globals: {
     __DEV__: true,
   },
   testEnvironment: 'jest-environment-jsdom-fourteen',
-  globalSetup: 'jest-playwright-preset/setup',
-  globalTeardown: 'jest-playwright-preset/teardown',
   setupFilesAfterEnv: ['<rootDir>/tests/setupTests.js'],
   reporters: ['default', require.resolve('../tests/imageReporter.js')],
   setupFiles: ['dotenv/config'],
