5fe286a0cffac5b53ab097528e610b627d049df3
Federico Zivolo <federico.zivolo@gmail.com>
2020-04-12 15:34:52 +0200

    chore: move jest-puppeteer config to .config folder

--- .config/jest.config.js
+++ .config/jest.config.js
@@ -1,3 +1,8 @@
+// https://github.com/smooth-code/jest-puppeteer/issues/160#issuecomment-491975158
+process.env.JEST_PUPPETEER_CONFIG = require.resolve(
+  './jest-puppeteer.config.js'
+);
+
 module.exports = {
   preset: 'jest-puppeteer',
   testMatch: ['<rootDir>/src/**/*.test.js', '<rootDir>/tests/**/*.test.js'],
