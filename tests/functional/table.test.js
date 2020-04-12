ff8d43255ebb7e7385ad3366d3d78b2e7165e75b
Federico Zivolo <federico.zivolo@gmail.com>
2020-04-12 07:47:17 +0200

    test: workaround for table visual tests

--- tests/functional/table.test.js
+++ tests/functional/table.test.js
@@ -4,9 +4,17 @@
  */
 import { screenshot, scroll } from '../utils/puppeteer.js';
 
+const hack = async page => {
+  // HACK: fixes issue with tables on GitHub Actions
+  if (Boolean(process.env.CI)) {
+    await page.addStyleTag({ content: 'table { margin-left: 4px; }' });
+  }
+};
+
 it('should position popper on right when reference is in table', async () => {
   const page = await browser.newPage();
   await page.goto(`${TEST_URL}/table/basic.html`);
+  await hack(page);
 
   await scroll(page, 'html', 100);
 
@@ -16,6 +24,7 @@ it('should position popper on right when reference is in table', async () => {
 it('should position popper on right when reference and popper are in table', async () => {
   const page = await browser.newPage();
   await page.goto(`${TEST_URL}/table/same.html`);
+  await hack(page);
 
   await scroll(page, 'html', 100);
 
@@ -25,6 +34,7 @@ it('should position popper on right when reference and popper are in table', asy
 it('should position popper on right when reference is in table inside offsetParents', async () => {
   const page = await browser.newPage();
   await page.goto(`${TEST_URL}/table/offset-parent.html`);
+  await hack(page);
 
   await scroll(page, 'html', 100);
 
@@ -34,6 +44,7 @@ it('should position popper on right when reference is in table inside offsetPare
 it('should position popper on right when reference and popper are in table inside offsetParents', async () => {
   const page = await browser.newPage();
   await page.goto(`${TEST_URL}/table/same-offset-parent.html`);
+  await hack(page);
 
   await scroll(page, 'html', 100);
 
