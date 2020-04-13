f239773694093bc4dbd3815e26b55b91a311f7b8
atomiks <cc.glows@gmail.com>
2020-04-13 23:07:52 +1000

    fix: only apply to iOS

--- src/dom-utils/getViewportRect.js
+++ src/dom-utils/getViewportRect.js
@@ -7,19 +7,14 @@ export default function getViewportRect(element: Element) {
 
   let width = win.innerWidth;
   let height = win.innerHeight;
-  let x = 0;
-  let y = 0;
 
-  if (visualViewport) {
+  // We don't know which browsers have buggy or odd implementations of this, so
+  // for now we're only applying it to iOS to fix the keyboard issue.
+  // Investigation required
+  if (visualViewport && /iPhone|iPod|iPad/.test(navigator.platform)) {
     width = visualViewport.width;
     height = visualViewport.height;
-
-    // iOS has a buggy implementation as expected
-    if (!/iPhone|iPod|iPad/.test(navigator.platform)) {
-      x = visualViewport.offsetLeft;
-      y = visualViewport.offsetTop;
-    }
   }
 
-  return { width, height, x, y };
+  return { width, height, x: 0, y: 0 };
 }
