5dd9d68d93e510cfa6527428e9d0eed9e04e0213
atomiks <cc.glows@gmail.com>
2020-04-20 23:33:18 +1000

    fix: add warning for SVGElement as arrow

---
+++ src/modifiers/arrow.test.js
@@ -0,0 +1,36 @@
+// @flow
+import { createPopper } from '../../src/popper';
+
+it('errors if SVGElement is used as arrow', () => {
+  const reference = document.createElement('div');
+  const popper = document.createElement('div');
+  const arrow = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+
+  popper.appendChild(arrow);
+
+  createPopper(reference, popper, {
+    modifiers: [{ name: 'arrow', options: { element: arrow } }],
+  });
+
+  expect(console.error).toHaveBeenCalledWith(
+    [
+      'Popper: "arrow" element must be an HTMLElement (not an SVGElement).',
+      'To use an SVG arrow, wrap it in a HTMLElement that will be used as',
+      'the arrow.',
+    ].join(' ')
+  );
+});
+
+it('does not error if HTMLElement is used as arrow', () => {
+  const reference = document.createElement('div');
+  const popper = document.createElement('div');
+  const arrow = document.createElement('div');
+
+  popper.appendChild(arrow);
+
+  createPopper(reference, popper, {
+    modifiers: [{ name: 'arrow', options: { element: arrow } }],
+  });
+
+  expect(console.error).not.toHaveBeenCalled();
+});
