9f720da6aa6d4f72e9c3a6f26389f95eadf30cf6
Jacob Wright <jacwright@gmail.com>
2020-07-30 11:06:52 -0600

    Fix errors when iFrames are destroyed
    Errors are thrown when an iFrame is destroyed because the popper's reference node's document no longer has a `defaultView`. The errors being thrown are:
    
    > Cannot read property 'Element' of null
    
    and
    
    > Cannot read property 'HTMLElement' of null
    
    from within `update`.

--- src/dom-utils/getWindow.js
+++ src/dom-utils/getWindow.js
@@ -5,7 +5,7 @@
 export default function getWindow(node) {
   if (node.toString() !== '[object Window]') {
     const ownerDocument = node.ownerDocument;
-    return ownerDocument ? ownerDocument.defaultView : window;
+    return ownerDocument ? ownerDocument.defaultView || window : window;
   }
 
   return node;
