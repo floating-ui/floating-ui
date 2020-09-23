a1e867e80bd067c18e76ca69ba3a1bd56e9fa6e4
Jason Spangler <jason.spangler@skuid.com>
2020-09-23 07:38:02 -0400

    Separate const statements

--- src/dom-utils/contains.js
+++ src/dom-utils/contains.js
@@ -1,8 +1,8 @@
 // @flow
 export default function contains(parent: Element, child: Element) {
-  const rootNode = child.getRootNode && child.getRootNode(),
-    // $FlowFixMe: Node is not aware of host
-    isShadow = Boolean(rootNode && rootNode.host);
+  const rootNode = child.getRootNode && child.getRootNode();
+  // $FlowFixMe: Node is not aware of host
+  const isShadow = Boolean(rootNode && rootNode.host);
 
   // First, attempt with faster native method
   if (parent.contains(child)) {
