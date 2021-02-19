da0a31b4ee09d93d82ed7f085d6b5e09f4b8586c
Mateusz Daniluk <danilukmateusz92@gmail.com>
2021-02-19 15:47:16 +0100

    fix: will-change create containig block

--- src/dom-utils/getOffsetParent.js
+++ src/dom-utils/getOffsetParent.js
@@ -34,7 +34,7 @@ function getContainingBlock(element: Element) {
     if (
       css.transform !== 'none' ||
       css.perspective !== 'none' ||
-      (css.willChange && css.willChange !== 'auto')
+      (css.willChange && (css.willChange === 'transform' || css.willChange === 'perspective'))
     ) {
       return currentNode;
     } else {
