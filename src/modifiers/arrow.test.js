9bd56290ef5caa70f156172a0399394bf2482a17
Federico Zivolo <federico.zivolo@gmail.com>
2021-02-24 14:29:28 +0100

    fix: pass fresh data to the arrow padding fn

--- src/modifiers/arrow.test.js
+++ src/modifiers/arrow.test.js
@@ -50,13 +50,10 @@ it('allows to define a function as padding', () => {
       },
     ],
   });
+  instance.forceUpdate();
 
-  expect(
-    instance.state.modifiersData['arrow#persistent'].padding
-  ).toMatchObject({
-    bottom: 10,
-    left: 10,
-    right: 10,
-    top: 10,
+  expect(instance.state.modifiersData['arrow']).toMatchObject({
+    centerOffset: 10,
+    x: 10,
   });
 });
