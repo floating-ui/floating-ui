553a57291b360e7039b4ae308702b6432453db65
James N <cc.glows@gmail.com>
2021-03-02 04:06:40 +1100

    fix: use 1px check for layout rect

--- src/dom-utils/getLayoutRect.js
+++ src/dom-utils/getLayoutRect.js
@@ -12,11 +12,11 @@ export default function getLayoutRect(element: HTMLElement): Rect {
   let width = element.offsetWidth;
   let height = element.offsetHeight;
 
-  if (Math.abs(clientRect.width - width) <= 0.5) {
+  if (Math.abs(clientRect.width - width) <= 1) {
     width = clientRect.width;
   }
 
-  if (Math.abs(clientRect.height - height) <= 0.5) {
+  if (Math.abs(clientRect.height - height) <= 1) {
     height = clientRect.height;
   }
 
