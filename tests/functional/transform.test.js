03605ee0c2f7f78bfc1a0f6a2d9bca6c1707f254
Tyson Holmes <tyson.holmes@rms.com>
2021-08-02 10:45:44 -0600

    fix: dynamically include scale in bounding client rect (#1322)

--- tests/functional/transform.test.js
+++ tests/functional/transform.test.js
@@ -4,9 +4,30 @@
  */
 import { screenshot } from '../utils/puppeteer.js';
 
-it('should position the popper on the right', async () => {
+it('should position the popper on the right when the reference element is scaled', async () => {
   const page = await browser.newPage();
-  await page.goto(`${TEST_URL}/transform.html`);
+  await page.goto(`${TEST_URL}/transform/reference-scaled.html`);
+
+  expect(await screenshot(page)).toMatchImageSnapshot();
+});
+
+it('should position the popper on the right when the popper element is scaled', async () => {
+  const page = await browser.newPage();
+  await page.goto(`${TEST_URL}/transform/popper-scaled.html`);
+
+  expect(await screenshot(page)).toMatchImageSnapshot();
+});
+
+it('should position the popper on the right when the parent element is scaled', async () => {
+  const page = await browser.newPage();
+  await page.goto(`${TEST_URL}/transform/parent-scaled.html`);
+
+  expect(await screenshot(page)).toMatchImageSnapshot();
+});
+
+it('should position the popper on the right when the reference element and popper element have different scales', async () => {
+  const page = await browser.newPage();
+  await page.goto(`${TEST_URL}/transform/different-scales.html`);
 
   expect(await screenshot(page)).toMatchImageSnapshot();
 });
