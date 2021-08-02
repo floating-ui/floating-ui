1ba85e8513c5bf241e0b81a787e763fa1e2f40cc
Tyson Holmes <tyson.holmes@rms.com>
2021-08-02 16:55:40 -0600

    only try to access Element properties if isHTMLElement

--- src/dom-utils/getCompositeRect.js
+++ src/dom-utils/getCompositeRect.js
@@ -9,11 +9,14 @@ import getDocumentElement from './getDocumentElement';
 import isScrollParent from './isScrollParent';
 
 function isElementScaled(element) {
-  const rect = element.getBoundingClientRect();
-  const scaleX = rect.width / element.offsetWidth || 1;
-  const scaleY = rect.height / element.offsetHeight || 1;
+  if (isHTMLElement(element)) {
+    const rect = element.getBoundingClientRect();
+    const scaleX = rect.width / element.offsetWidth || 1;
+    const scaleY = rect.height / element.offsetHeight || 1;
 
-  return scaleX !== 1 || scaleY !== 1;
+    return scaleX !== 1 || scaleY !== 1;
+  }
+  return false;
 }
 
 // Returns the composite rect of an element relative to its offsetParent.
@@ -24,10 +27,13 @@ export default function getCompositeRect(
   isFixed: boolean = false
 ): Rect {
   const isOffsetParentAnElement = isHTMLElement(offsetParent);
-  const offsetParentIsScaled = isOffsetParentAnElement && isElementScaled(offsetParent);
+  const offsetParentIsScaled =
+    isOffsetParentAnElement && isElementScaled(offsetParent);
   const documentElement = getDocumentElement(offsetParent);
-  const rect = getBoundingClientRect(elementOrVirtualElement, offsetParentIsScaled);
-
+  const rect = getBoundingClientRect(
+    elementOrVirtualElement,
+    offsetParentIsScaled
+  );
 
   let scroll = { scrollLeft: 0, scrollTop: 0 };
   let offsets = { x: 0, y: 0 };
@@ -41,7 +47,7 @@ export default function getCompositeRect(
       scroll = getNodeScroll(offsetParent);
     }
 
-    if (isOffsetParentAnElement) {
+    if (isHTMLElement(offsetParent)) {
       offsets = getBoundingClientRect(offsetParent, true);
       offsets.x += offsetParent.clientLeft;
       offsets.y += offsetParent.clientTop;
