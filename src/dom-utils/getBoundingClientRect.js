d6fe3365a9b5058887d8b9082458f754a72de9d8
James Nelson <cc.glows@gmail.com>
2021-11-03 12:29:17 +1100

    fix(getBoundingClientRect): reenable scale logic
    fix: reenable scaled parent logic
    
    test: update snapshot
    
    test: upload to imgbb
    
    fix

--- src/dom-utils/getBoundingClientRect.js
+++ src/dom-utils/getBoundingClientRect.js
@@ -1,34 +1,29 @@
 // @flow
 import type { ClientRectObject, VirtualElement } from '../types';
-// import { isHTMLElement } from './instanceOf';
+import { isHTMLElement } from './instanceOf';
+import { round } from '../utils/math';
 
 export default function getBoundingClientRect(
   element: Element | VirtualElement,
-  // eslint-disable-next-line unused-imports/no-unused-vars
   includeScale: boolean = false
 ): ClientRectObject {
   const rect = element.getBoundingClientRect();
   let scaleX = 1;
   let scaleY = 1;
 
-  // FIXME:
-  // `offsetWidth` returns an integer while `getBoundingClientRect`
-  // returns a float. This results in `scaleX` or `scaleY` being
-  // non-1 when it should be for elements that aren't a full pixel in
-  // width or height.
-  // if (isHTMLElement(element) && includeScale) {
-  //   const offsetHeight = element.offsetHeight;
-  //   const offsetWidth = element.offsetWidth;
+  if (isHTMLElement(element) && includeScale) {
+    const offsetHeight = element.offsetHeight;
+    const offsetWidth = element.offsetWidth;
 
-  //   // Do not attempt to divide by 0, otherwise we get `Infinity` as scale
-  //   // Fallback to 1 in case both values are `0`
-  //   if (offsetWidth > 0) {
-  //     scaleX = rect.width / offsetWidth || 1;
-  //   }
-  //   if (offsetHeight > 0) {
-  //     scaleY = rect.height / offsetHeight || 1;
-  //   }
-  // }
+    // Do not attempt to divide by 0, otherwise we get `Infinity` as scale
+    // Fallback to 1 in case both values are `0`
+    if (offsetWidth > 0) {
+      scaleX = round(rect.width) / offsetWidth || 1;
+    }
+    if (offsetHeight > 0) {
+      scaleY = round(rect.height) / offsetHeight || 1;
+    }
+  }
 
   return {
     width: rect.width / scaleX,
