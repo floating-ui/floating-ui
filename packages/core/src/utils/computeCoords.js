88d291aa347e9d138b5b761a5ea928b41241e305
James Nelson <cc.glows@gmail.com>
2021-11-21 01:54:44 +1100

    refactor: unneccessary checks

--- packages/core/src/utils/computeCoords.js
+++ packages/core/src/utils/computeCoords.js
@@ -10,12 +10,12 @@ export default function computeCoords({
   popper,
   placement,
 }: {
-  reference: Rect | ClientRectObject,
-  popper: Rect | ClientRectObject,
-  placement?: Placement,
+  reference: Rect,
+  popper: Rect,
+  placement: Placement,
 }): Coords {
-  const basePlacement = placement ? getBasePlacement(placement) : null;
-  const variation = placement ? getVariation(placement) : null;
+  const basePlacement = getBasePlacement(placement);
+  const variation = getVariation(placement);
   const commonX = reference.x + reference.width / 2 - popper.width / 2;
   const commonY = reference.y + reference.height / 2 - popper.height / 2;
 
@@ -52,24 +52,19 @@ export default function computeCoords({
       };
   }
 
-  const mainAxis = basePlacement
-    ? getMainAxisFromPlacement(basePlacement)
-    : null;
+  const mainAxis = getMainAxisFromPlacement(basePlacement);
+  const len = mainAxis === 'y' ? 'height' : 'width';
 
-  if (mainAxis != null) {
-    const len = mainAxis === 'y' ? 'height' : 'width';
-
-    switch (variation) {
-      case start:
-        coords[mainAxis] =
-          coords[mainAxis] - (reference[len] / 2 - popper[len] / 2);
-        break;
-      case end:
-        coords[mainAxis] =
-          coords[mainAxis] + (reference[len] / 2 - popper[len] / 2);
-        break;
-      default:
-    }
+  switch (variation) {
+    case start:
+      coords[mainAxis] =
+        coords[mainAxis] - (reference[len] / 2 - popper[len] / 2);
+      break;
+    case end:
+      coords[mainAxis] =
+        coords[mainAxis] + (reference[len] / 2 - popper[len] / 2);
+      break;
+    default:
   }
 
   return coords;
