bb366e6472963359047c2676ebfda60b616137a1
James Nelson <cc.glows@gmail.com>
2021-12-21 12:30:26 +1100

    remove firefox for now

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,10 +17,10 @@ const config: PlaywrightTestConfig = {
       name: 'chromium',
       use: {...devices['Desktop Chrome']},
     },
-    {
-      name: 'firefox',
-      use: {...devices['Desktop Firefox']},
-    },
+    // {
+    //   name: 'firefox',
+    //   use: {...devices['Desktop Firefox']},
+    // },
     {
       name: 'webkit',
       use: {...devices['Desktop Safari']},
