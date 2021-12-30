21c636e045c81e2d39ca2e01fc11d01c1f0d71a8
James N <cc.glows@gmail.com>
2021-12-30 15:12:11 +1100

    Revert #1399
    skip 1399 test

--- src/dom-utils/getClippingRect.js
+++ src/dom-utils/getClippingRect.js
@@ -63,10 +63,7 @@ function getClippingParents(element: Element): Array<Element> {
     (clippingParent) =>
       isElement(clippingParent) &&
       contains(clippingParent, clipperElement) &&
-      getNodeName(clippingParent) !== 'body' &&
-      (canEscapeClipping
-        ? getComputedStyle(clippingParent).position !== 'static'
-        : true)
+      getNodeName(clippingParent) !== 'body'
   );
 }
 
