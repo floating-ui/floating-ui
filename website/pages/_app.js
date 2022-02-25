7624ce245636de17434282f42202baf2008b35a1
James Nelson <cc.glows@gmail.com>
2022-02-25 15:26:50 +1100

    docs: update for new release

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -3,10 +3,21 @@ import '@reach/skip-nav/styles.css';
 import '../assets/global.css';
 
 import {useAnalytics} from '../lib/hooks/useAnalytics';
+import Layout from '../lib/components/Layout';
+import {useRouter} from 'next/router';
+import {Fragment} from 'react';
 
 function MyApp({Component, pageProps}) {
+  const {pathname} = useRouter();
+  const Wrapper = pathname === '/' ? Fragment : Layout;
+
   useAnalytics();
-  return <Component {...pageProps} />;
+
+  return (
+    <Wrapper>
+      <Component {...pageProps} />
+    </Wrapper>
+  );
 }
 
 export default MyApp;
