72c9a49cc68d5765216273f9321fdd7c6c97ea96
James Nelson <cc.glows@gmail.com>
2022-03-12 03:43:27 +1100

    fix(dom): type elements

--- packages/dom/index.test-d.ts
+++ packages/dom/index.test-d.ts
@@ -1,4 +1,3 @@
-import {Middleware} from '@floating-ui/core';
 import {
   computePosition,
   shift,
@@ -10,6 +9,7 @@ import {
   size,
   arrow,
   detectOverflow,
+  Middleware,
 } from '.';
 
 // @ts-expect-error
@@ -168,7 +168,9 @@ const middlewareWithoutName: Middleware = {
 
 const middleware: Middleware = {
   name: 'test',
-  fn() {
+  fn(args) {
+    const {elements} = args;
+    elements.floating;
     return {};
   },
 };
