cd70d8d5f052f91ba0fecbbba5943c930f9eb89f
James N <cc.glows@gmail.com>
2022-04-12 20:22:05 +1000

    refactor(useInteractions): simplify merging logic (#1614)

---
+++ packages/react-dom-interactions/test/unit/useInteractions.test.ts
@@ -0,0 +1,46 @@
+import {useInteractions} from '../../src/useInteractions';
+
+test('returns prop getters', () => {
+  const {getReferenceProps, getFloatingProps, getItemProps} = useInteractions(
+    []
+  );
+
+  expect(typeof getReferenceProps).toBe('function');
+  expect(typeof getFloatingProps).toBe('function');
+  expect(typeof getItemProps).toBe('function');
+});
+
+test('correctly merges functions', () => {
+  const firstInteractionOnClick = jest.fn();
+  const secondInteractionOnClick = jest.fn();
+  const secondInteractionOnKeyDown = jest.fn();
+  const userOnClick = jest.fn();
+
+  const {getReferenceProps} = useInteractions([
+    {reference: {onClick: firstInteractionOnClick}},
+    {
+      reference: {
+        onClick: secondInteractionOnClick,
+        onKeyDown: secondInteractionOnKeyDown,
+      },
+    },
+  ]);
+
+  const {onClick, onKeyDown} = getReferenceProps({onClick: userOnClick});
+
+  onClick();
+  onKeyDown();
+
+  expect(firstInteractionOnClick).toHaveBeenCalledTimes(1);
+  expect(secondInteractionOnClick).toHaveBeenCalledTimes(1);
+  expect(userOnClick).toHaveBeenCalledTimes(1);
+  expect(secondInteractionOnKeyDown).toHaveBeenCalledTimes(1);
+});
+
+test('does not error with undefined user supplied functions', () => {
+  const {getReferenceProps} = useInteractions([{reference: {onClick() {}}}]);
+
+  expect(() =>
+    getReferenceProps({onClick: undefined}).onClick()
+  ).not.toThrowError();
+});
