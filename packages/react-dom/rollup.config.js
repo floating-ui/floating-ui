c2de18c7a39d7be96e493af361e8f3a1edfa8c26
James Nelson <cc.glows@gmail.com>
2022-05-06 10:41:24 +1000

    missing .mjs

--- packages/react-dom/rollup.config.js
+++ packages/react-dom/rollup.config.js
@@ -53,8 +53,8 @@ const bundles = [
   {
     input,
     output: {
-      file: path.join(__dirname, 'dist/floating-ui.react-dom.cjs'),
-      format: 'cjs',
+      file: path.join(__dirname, 'dist/floating-ui.react-dom.mjs'),
+      format: 'esm',
     },
   },
 ];
