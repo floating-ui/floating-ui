b9680c458e75150829854eb004f9d743a7b7a47f
Jaril <jarilvalenciano@gmail.com>
2022-07-13 21:33:00 -0700

    Adding action-playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
-import {PlaywrightTestConfig} from '@playwright/test';
+import { PlaywrightTestConfig, devices } from '@playwright/test';
+import { devices as replayDevices } from "@replayio/playwright";
 
 const config: PlaywrightTestConfig = {
   use: {
@@ -12,6 +13,24 @@ const config: PlaywrightTestConfig = {
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
+  projects: [
+    {
+      name: "replay-firefox",
+      use: { ...replayDevices["Replay Firefox"] as any },
+    },
+    {
+      name: "replay-chromium",
+      use: { ...replayDevices["Replay Chromium"] as any },
+    },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chromium"] },
+    },
+  ],
 };
 
 export default config;
