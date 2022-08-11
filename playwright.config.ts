38583076f5d4aca4abba9de7e9fb7e93d2b5d139
Jason Laster <jason.laster.11@gmail.com>
2022-08-10 20:19:23 -0700

    fixtures

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,5 @@
 import {PlaywrightTestConfig, devices} from '@playwright/test';
-import { devices as replayDevices } from "@replayio/playwright";
+import {devices as replayDevices} from '@replayio/playwright';
 
 const config: PlaywrightTestConfig = {
   use: {
@@ -15,20 +15,20 @@ const config: PlaywrightTestConfig = {
   },
   projects: [
     {
-      name: "replay-firefox",
-      use: { ...replayDevices["Replay Firefox"] as any },
+      name: 'replay-firefox',
+      use: {...(replayDevices['Replay Firefox'] as any)},
     },
+    // {
+    //   name: "replay-chromium",
+    //   use: { ...replayDevices["Replay Chromium"] as any },
+    // },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
     {
-      name: "replay-chromium",
-      use: { ...replayDevices["Replay Chromium"] as any },
-    },
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chromium"] },
+      name: 'chromium',
+      use: {...devices['Desktop Chromium']},
     },
   ],
 };
