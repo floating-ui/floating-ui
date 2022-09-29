3dd6b42489b2beaeb97379769a6e4d63333ee171
Sven <sventschui@gmail.com>
2022-09-29 08:08:55 +0200

    fix: do not treat elements with display: inline or display: contents as overflow ancestors (#1847)
    Co-authored-by: Sven Tschui <sven.tschui@ti8m.ch>

--- packages/dom/test/unit/getOverflowAncestors.test.ts
+++ packages/dom/test/unit/getOverflowAncestors.test.ts
@@ -16,3 +16,52 @@ test('returns all overflow ancestors', () => {
     window,
   ]);
 });
+
+test('does not treat display: inline elements as overflow ancestors', () => {
+  const overflowScroll = document.createElement('div');
+  overflowScroll.style.overflow = 'scroll';
+  overflowScroll.style.display = 'inline';
+  const overflowHidden = document.createElement('div');
+  overflowHidden.style.overflow = 'hidden';
+  overflowHidden.style.display = 'inline';
+  const test = document.createElement('div');
+
+  overflowScroll.append(overflowHidden);
+  overflowHidden.append(test);
+
+  expect(getOverflowAncestors(test)).toEqual([window]);
+});
+
+test('does not treat display: contents elements as overflow ancestors', () => {
+  const overflowScroll = document.createElement('div');
+  overflowScroll.style.overflow = 'scroll';
+  overflowScroll.style.display = 'contents';
+  const overflowHidden = document.createElement('div');
+  overflowHidden.style.overflow = 'hidden';
+  overflowHidden.style.display = 'contents';
+  const test = document.createElement('div');
+
+  overflowScroll.append(overflowHidden);
+  overflowHidden.append(test);
+
+  expect(getOverflowAncestors(test)).toEqual([window]);
+});
+
+test('does treat display: inline-block elements as overflow ancestors', () => {
+  const overflowScroll = document.createElement('div');
+  overflowScroll.style.overflow = 'scroll';
+  overflowScroll.style.display = 'inline-block';
+  const overflowHidden = document.createElement('div');
+  overflowHidden.style.overflow = 'hidden';
+  overflowHidden.style.display = 'inline-block';
+  const test = document.createElement('div');
+
+  overflowScroll.append(overflowHidden);
+  overflowHidden.append(test);
+
+  expect(getOverflowAncestors(test)).toEqual([
+    overflowHidden,
+    overflowScroll,
+    window,
+  ]);
+});
