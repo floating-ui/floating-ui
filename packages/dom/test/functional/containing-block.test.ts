bfeb638600877259cb7c8377f815b93fe7dc7fd3
James Nelson <cc.glows@gmail.com>
2022-10-19 15:51:26 +1100

    make exclusive

--- packages/dom/test/functional/containing-block.test.ts
+++ packages/dom/test/functional/containing-block.test.ts
@@ -5,6 +5,9 @@ import {click} from './utils/click';
   (willChange) => {
     test(`should be positioned on bottom ${willChange}`, async ({page}) => {
       await page.goto('http://localhost:1234/containing-block');
+
+      // Ensure `contain` does not affect `will-change`
+      await click(page, `[data-testid="contain-size"]`);
       await click(page, `[data-testid="willchange-${willChange}"]`);
 
       expect(await page.locator('.container').screenshot()).toMatchSnapshot(
@@ -18,6 +21,9 @@ import {click} from './utils/click';
   (contain) => {
     test(`should be positioned on bottom ${contain}`, async ({page}) => {
       await page.goto('http://localhost:1234/containing-block');
+
+      // Ensure `will-change` does not affect `contain`
+      await click(page, `[data-testid="willchange-opacity"]`);
       await click(page, `[data-testid="contain-${contain}"]`);
 
       expect(await page.locator('.container').screenshot()).toMatchSnapshot(
