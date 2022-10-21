ba0b42f007b7eb87d465d1cbf90380ac319b7263
James Nelson <cc.glows@gmail.com>
2022-10-22 10:59:26 +1100

    test: add virtual element route

---
+++ packages/dom/test/visual/spec/VirtualElement.tsx
@@ -0,0 +1,56 @@
+import {autoUpdate, useFloating} from '@floating-ui/react-dom';
+import {useEffect, useRef} from 'react';
+import {useScroll} from '../utils/useScroll';
+
+export function VirtualElement() {
+  const referenceToDeriveRef = useRef<HTMLDivElement>(null);
+  const {x, y, reference, floating, strategy, update, refs} = useFloating({
+    strategy: 'fixed',
+    whileElementsMounted: autoUpdate,
+  });
+
+  const {scrollRef} = useScroll({refs, update});
+
+  useEffect(() => {
+    const contextElement = referenceToDeriveRef.current;
+    if (contextElement) {
+      reference({
+        contextElement,
+        getBoundingClientRect() {
+          return contextElement.getBoundingClientRect();
+        },
+      });
+    }
+  }, [reference]);
+
+  return (
+    <>
+      <h1>Virtual Element</h1>
+      <p></p>
+      <div className="container">
+        <div
+          className="scroll"
+          data-x
+          style={{position: 'relative'}}
+          ref={scrollRef}
+        >
+          <div ref={referenceToDeriveRef} className="reference">
+            Reference
+          </div>
+        </div>
+      </div>
+
+      <div
+        ref={floating}
+        className="floating"
+        style={{
+          position: strategy,
+          top: y ?? 0,
+          left: x ?? 0,
+        }}
+      >
+        Floating
+      </div>
+    </>
+  );
+}
