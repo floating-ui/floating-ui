bb6314d588cb7ad1f227081d46fde08c3270ad30
James Nelson <cc.glows@gmail.com>
2022-12-16 11:47:04 +1100

    add content-box to test

--- packages/dom/test/functional/border.test.ts
+++ packages/dom/test/functional/border.test.ts
@@ -1,16 +1,22 @@
 import {test, expect} from '@playwright/test';
 import {click} from './utils/click';
 
-[null, 'reference', 'floating', 'body', 'html', 'offsetParent'].forEach(
-  (node) => {
-    test(`correctly positioned on bottom when ${node} has a border`, async ({
-      page,
-    }) => {
-      await page.goto('http://localhost:1234/border');
-      await click(page, `[data-testid="border-${node}"]`);
-      expect(await page.locator('.container').screenshot()).toMatchSnapshot(
-        `${node}.png`
-      );
-    });
-  }
-);
+[
+  null,
+  'reference',
+  'floating',
+  'body',
+  'html',
+  'offsetParent',
+  'content-box',
+].forEach((node) => {
+  test(`correctly positioned on bottom when ${node} has a border`, async ({
+    page,
+  }) => {
+    await page.goto('http://localhost:1234/border');
+    await click(page, `[data-testid="border-${node}"]`);
+    expect(await page.locator('.container').screenshot()).toMatchSnapshot(
+      `${node}.png`
+    );
+  });
+});
