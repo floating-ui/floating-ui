d76269292ccde6bc78765b28a774989a971f6f14
James Nelson <cc.glows@gmail.com>
2022-12-25 22:25:08 +1100

    keep full overflow check

--- packages/dom/src/utils/is.ts
+++ packages/dom/src/utils/is.ts
@@ -35,9 +35,9 @@ export function isShadowRoot(node: Node): node is ShadowRoot {
 }
 
 export function isOverflowElement(element: Element): boolean {
-  const {overflowX, overflowY, display} = getComputedStyle(element);
+  const {overflow, overflowX, overflowY, display} = getComputedStyle(element);
   return (
-    /auto|scroll|overlay|hidden|clip/.test(overflowY + overflowX) &&
+    /auto|scroll|overlay|hidden|clip/.test(overflow + overflowY + overflowX) &&
     !['inline', 'contents'].includes(display)
   );
 }
