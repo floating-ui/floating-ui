dd4df64fe1e3bd522fe0b40eded0fd1049da7f09
James Nelson <cc.glows@gmail.com>
2022-12-25 22:05:58 +1100

    fix(isOverflowElement): check for clip value

--- packages/dom/src/utils/is.ts
+++ packages/dom/src/utils/is.ts
@@ -35,10 +35,9 @@ export function isShadowRoot(node: Node): node is ShadowRoot {
 }
 
 export function isOverflowElement(element: Element): boolean {
-  // Firefox wants us to check `-x` and `-y` variations as well
-  const {overflow, overflowX, overflowY, display} = getComputedStyle(element);
+  const {overflowX, overflowY, display} = getComputedStyle(element);
   return (
-    /auto|scroll|overlay|hidden/.test(overflow + overflowY + overflowX) &&
+    /auto|scroll|overlay|hidden|clip/.test(overflowY + overflowX) &&
     !['inline', 'contents'].includes(display)
   );
 }
