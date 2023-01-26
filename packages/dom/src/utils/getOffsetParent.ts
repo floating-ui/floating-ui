73eb99bd186ac3eeb8feaa55da63f43d05efcba5
Cory LaViska <cory@abeautifulsite.net>
2023-01-26 10:32:07 -0500

    export composedOffsetParent for convenience

--- packages/dom/src/utils/getOffsetParent.ts
+++ packages/dom/src/utils/getOffsetParent.ts
@@ -5,6 +5,7 @@ import {
   isContainingBlock,
   isHTMLElement,
   isLastTraversableNode,
+  isShadowRoot,
   isTableElement,
 } from './is';
 import {getWindow} from './window';
@@ -60,4 +61,52 @@ export function getOffsetParent(element: Element): Element | Window {
   }
 
   return offsetParent || getContainingBlock(element) || window;
+}
+
+/**
+ * Polyfills the old offsetParent behavior from before the spec was changed:
+ * https://github.com/w3c/csswg-drafts/issues/159
+ *
+ * This is exported as a convenience function for consumers that want to opt
+ * into the more accurate old behavior. Note that using this function in some
+ * DOM configurations may introduce performance issues.
+ */
+export function composedOffsetParent(element: HTMLElement): Element | null {
+  let {offsetParent} = element;
+
+  let ancestor: Element = element;
+  let foundInsideSlot = false;
+
+  while (ancestor && ancestor !== offsetParent) {
+    const {assignedSlot} = ancestor;
+
+    if (assignedSlot) {
+      let newOffsetParent = assignedSlot.offsetParent;
+
+      if (getComputedStyle(assignedSlot).display === 'contents') {
+        const hadStyleAttribute = assignedSlot.hasAttribute('style');
+        const oldDisplay = assignedSlot.style.display;
+        assignedSlot.style.display = getComputedStyle(ancestor).display;
+
+        newOffsetParent = assignedSlot.offsetParent;
+
+        assignedSlot.style.display = oldDisplay;
+        if (!hadStyleAttribute) {
+          assignedSlot.removeAttribute('style');
+        }
+      }
+
+      ancestor = assignedSlot;
+      if (offsetParent !== newOffsetParent) {
+        offsetParent = newOffsetParent;
+        foundInsideSlot = true;
+      }
+    } else if (isShadowRoot(ancestor) && ancestor.host && foundInsideSlot) {
+      break;
+    }
+    ancestor = ((isShadowRoot(ancestor) && ancestor.host) ||
+      ancestor.parentNode) as Element;
+  }
+
+  return offsetParent;
 }
