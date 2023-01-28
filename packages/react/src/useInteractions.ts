5fe42f73ce1a1344eca25f319ac18b7816704058
atomiks <cc.glows@gmail.com>
2023-01-28 19:31:27 +1100

    perf: make `getItemProps` change less frequently (#2147)

--- packages/react/src/useInteractions.ts
+++ packages/react/src/useInteractions.ts
@@ -65,8 +65,12 @@ export const useInteractions = (propsList: Array<ElementProps | void> = []) => {
   const getItemProps = React.useCallback(
     (userProps?: React.HTMLProps<HTMLElement>) =>
       mergeProps(userProps, propsList, 'item'),
+    // Granularly check for `item` changes, because the `getItemProps` getter
+    // should be as referentially stable as possible since it may be passed as
+    // a prop to many components. All `item` key values must therefore be
+    // memoized.
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    deps
+    propsList.map((key) => key?.item)
   );
 
   return React.useMemo(
