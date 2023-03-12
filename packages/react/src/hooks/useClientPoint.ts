02c127ad9d10800e0ef29e5df9bb458612e737bf
atomiks <cc.glows@gmail.com>
2023-03-13 02:15:28 +1100

    feat(react): `useClientPoint` hook (#2179)

---
+++ packages/react/src/hooks/useClientPoint.ts
@@ -0,0 +1,224 @@
+import * as React from 'react';
+import useLayoutEffect from 'use-isomorphic-layout-effect';
+
+import type {ElementProps, FloatingContext, ReferenceType} from '../types';
+import {contains} from '../utils/contains';
+import {getTarget} from '../utils/getTarget';
+import {getWindow, isMouseLikePointerType} from '../utils/is';
+import {useEvent} from './utils/useEvent';
+
+function createVirtualElement(
+  domRef: React.MutableRefObject<Element | null>,
+  data: {
+    axis: 'x' | 'y' | 'both';
+    pointerType: string | undefined;
+    x: number | null;
+    y: number | null;
+  }
+) {
+  let offsetX: number | null = null;
+  let offsetY: number | null = null;
+  let isAutoUpdateEvent = false;
+
+  return {
+    contextElement: domRef.current || undefined,
+    getBoundingClientRect() {
+      const domRect = domRef.current?.getBoundingClientRect() || {
+        width: 0,
+        height: 0,
+        x: 0,
+        y: 0,
+      };
+
+      const isXAxis = data.axis === 'x' || data.axis === 'both';
+      const isYAxis = data.axis === 'y' || data.axis === 'both';
+
+      let width = domRect.width;
+      let height = domRect.height;
+      let x = domRect.x;
+      let y = domRect.y;
+
+      if (offsetX == null && data.x && isXAxis) {
+        offsetX = domRect.x - data.x;
+      }
+
+      if (offsetY == null && data.y && isYAxis) {
+        offsetY = domRect.y - data.y;
+      }
+
+      x -= offsetX || 0;
+      y -= offsetY || 0;
+      width = 0;
+      height = 0;
+
+      if (!isAutoUpdateEvent || data.pointerType !== 'touch') {
+        width = data.axis === 'y' ? domRect.width : 0;
+        height = data.axis === 'x' ? domRect.height : 0;
+        x = isXAxis && data.x != null ? data.x : x;
+        y = isYAxis && data.y != null ? data.y : y;
+      }
+
+      if (isAutoUpdateEvent && data.pointerType === 'touch') {
+        height = data.axis === 'x' ? domRect.height : height;
+        width = data.axis === 'y' ? domRect.width : width;
+      }
+
+      isAutoUpdateEvent = true;
+
+      return {
+        width,
+        height,
+        x,
+        y,
+        top: y,
+        right: x + width,
+        bottom: y + height,
+        left: x,
+      };
+    },
+  };
+}
+
+function isMouseBasedEvent(event: Event | undefined): event is MouseEvent {
+  return event != null && (event as MouseEvent).clientX != null;
+}
+
+export interface Props {
+  enabled: boolean;
+  axis: 'x' | 'y' | 'both';
+  x: number | null;
+  y: number | null;
+}
+
+/**
+ * Positions the floating element relative to a client point (in the viewport),
+ * such as the mouse position. By default, it follows the mouse cursor.
+ * @see https://floating-ui.com/docs/useClientPoint
+ */
+export const useClientPoint = <RT extends ReferenceType = ReferenceType>(
+  {open, refs, dataRef, elements: {floating}}: FloatingContext<RT>,
+  {enabled = true, axis = 'both', x = null, y = null}: Partial<Props> = {}
+): ElementProps => {
+  const initialRef = React.useRef(false);
+  const cleanupListenerRef = React.useRef<null | (() => void)>(null);
+
+  const [pointerType, setPointerType] = React.useState<string | undefined>();
+  const [reactive, setReactive] = React.useState([]);
+
+  const setReference = useEvent((x: number | null, y: number | null) => {
+    if (initialRef.current) return;
+
+    // Prevent setting if the open event was not a mouse-like one
+    // (e.g. focus to open, then hover over the reference element).
+    // Only apply if the event exists.
+    if (
+      dataRef.current.openEvent &&
+      !isMouseBasedEvent(dataRef.current.openEvent)
+    ) {
+      return;
+    }
+
+    refs.setPositionReference(
+      createVirtualElement(refs.domReference, {
+        x,
+        y,
+        axis,
+        pointerType,
+      })
+    );
+  });
+
+  const handleReferenceEnterOrMove = useEvent(
+    (event: React.MouseEvent<Element>) => {
+      if (x != null || y != null) return;
+
+      if (!open) {
+        setReference(event.clientX, event.clientY);
+      } else if (!cleanupListenerRef.current) {
+        // If there's no cleanup, there's no listener, but we want to ensure
+        // we add the listener if the cursor landed on the floating element and
+        // then back on the reference (i.e. it's interactive).
+        setReactive([]);
+      }
+    }
+  );
+
+  // If the pointer is a mouse-like pointer, we want to continue following the
+  // mouse even if the floating element is transitioning out. On touch
+  // devices, this is undesirable because the floating element will move to
+  // the dismissal touch point.
+  const openCheck = isMouseLikePointerType(pointerType) ? floating : open;
+
+  const addListener = React.useCallback(() => {
+    // Explicitly specified `x`/`y` coordinates shouldn't add a listener.
+    if (!openCheck || !enabled || x != null || y != null) return;
+
+    const win = getWindow(refs.floating.current);
+
+    function handleMouseMove(event: MouseEvent) {
+      const target = getTarget(event) as Element | null;
+
+      if (!contains(refs.floating.current, target)) {
+        setReference(event.clientX, event.clientY);
+      } else {
+        win.removeEventListener('mousemove', handleMouseMove);
+        cleanupListenerRef.current = null;
+      }
+    }
+
+    if (
+      !dataRef.current.openEvent ||
+      isMouseBasedEvent(dataRef.current.openEvent)
+    ) {
+      win.addEventListener('mousemove', handleMouseMove);
+      const cleanup = () => {
+        win.removeEventListener('mousemove', handleMouseMove);
+        cleanupListenerRef.current = null;
+      };
+      cleanupListenerRef.current = cleanup;
+      return cleanup;
+    }
+
+    refs.setPositionReference(refs.domReference.current);
+  }, [dataRef, enabled, openCheck, refs, setReference, x, y]);
+
+  React.useEffect(() => {
+    return addListener();
+  }, [addListener, reactive]);
+
+  React.useEffect(() => {
+    if (enabled && !floating) {
+      initialRef.current = false;
+    }
+  }, [enabled, floating]);
+
+  React.useEffect(() => {
+    if (!enabled && open) {
+      initialRef.current = true;
+    }
+  }, [enabled, open]);
+
+  useLayoutEffect(() => {
+    if (enabled && (x != null || y != null)) {
+      initialRef.current = false;
+      setReference(x, y);
+    }
+  }, [enabled, x, y, setReference]);
+
+  return React.useMemo(() => {
+    if (!enabled) return {};
+
+    function setPointerTypeRef({pointerType}: React.PointerEvent) {
+      setPointerType(pointerType);
+    }
+
+    return {
+      reference: {
+        onPointerDown: setPointerTypeRef,
+        onPointerEnter: setPointerTypeRef,
+        onMouseMove: handleReferenceEnterOrMove,
+        onMouseEnter: handleReferenceEnterOrMove,
+      },
+    };
+  }, [enabled, handleReferenceEnterOrMove]);
+};
---
+++ packages/react/test/unit/useClientPoint.test.tsx
@@ -0,0 +1,293 @@
+import type {Coords} from '@floating-ui/react-dom';
+import {act, fireEvent, render, screen} from '@testing-library/react';
+import {useState} from 'react';
+
+import {useClientPoint, useFloating, useInteractions} from '../../src';
+
+function expectLocation({x, y}: Coords) {
+  expect(Number(screen.getByTestId('x')?.textContent)).toBe(x);
+  expect(Number(screen.getByTestId('y')?.textContent)).toBe(y);
+  expect(Number(screen.getByTestId('width')?.textContent)).toBe(0);
+  expect(Number(screen.getByTestId('height')?.textContent)).toBe(0);
+}
+
+function App({
+  enabled = true,
+  point,
+  axis,
+}: {
+  enabled?: boolean;
+  point?: Coords;
+  axis?: 'both' | 'x' | 'y';
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const {refs, elements, context} = useFloating({
+    open: isOpen,
+    onOpenChange: setIsOpen,
+  });
+  const clientPoint = useClientPoint(context, {
+    enabled,
+    ...point,
+    axis,
+  });
+  const {getReferenceProps, getFloatingProps} = useInteractions([clientPoint]);
+
+  const rect = elements.reference?.getBoundingClientRect();
+
+  return (
+    <>
+      <div
+        data-testid="reference"
+        ref={refs.setReference}
+        {...getReferenceProps()}
+      >
+        Reference
+      </div>
+      {isOpen && (
+        <div
+          data-testid="floating"
+          ref={refs.setFloating}
+          {...getFloatingProps()}
+        >
+          Floating
+        </div>
+      )}
+      <button onClick={() => setIsOpen((v) => !v)} />
+      <span data-testid="x">{rect?.x}</span>
+      <span data-testid="y">{rect?.y}</span>
+      <span data-testid="width">{rect?.width}</span>
+      <span data-testid="height">{rect?.height}</span>
+    </>
+  );
+}
+
+test('renders at explicit client point and can be updated', async () => {
+  const {rerender} = render(<App point={{x: 0, y: 0}} />);
+
+  fireEvent.click(screen.getByRole('button'));
+
+  await act(async () => {});
+
+  expectLocation({x: 0, y: 0});
+
+  rerender(<App point={{x: 1000, y: 1000}} />);
+  await act(async () => {});
+
+  expectLocation({x: 1000, y: 1000});
+});
+
+test('renders at mouse event coords', async () => {
+  render(<App />);
+
+  await act(async () => {});
+
+  fireEvent(
+    screen.getByTestId('reference'),
+    new MouseEvent('mousemove', {
+      bubbles: true,
+      clientX: 500,
+      clientY: 500,
+    })
+  );
+  await act(async () => {});
+
+  expectLocation({x: 500, y: 500});
+
+  fireEvent(
+    screen.getByTestId('reference'),
+    new MouseEvent('mousemove', {
+      bubbles: true,
+      clientX: 1000,
+      clientY: 1000,
+    })
+  );
+  await act(async () => {});
+
+  expectLocation({x: 1000, y: 1000});
+
+  // Window listener isn't registered unless the floating element is open.
+  fireEvent(
+    window,
+    new MouseEvent('mousemove', {
+      bubbles: true,
+      clientX: 700,
+      clientY: 700,
+    })
+  );
+  await act(async () => {});
+
+  expectLocation({x: 1000, y: 1000});
+
+  fireEvent.click(screen.getByRole('button'));
+  await act(async () => {});
+
+  fireEvent(
+    screen.getByTestId('reference'),
+    new MouseEvent('mousemove', {
+      bubbles: true,
+      clientX: 700,
+      clientY: 700,
+    })
+  );
+  await act(async () => {});
+
+  expectLocation({x: 700, y: 700});
+
+  fireEvent(
+    document.body,
+    new MouseEvent('mousemove', {
+      bubbles: true,
+      clientX: 0,
+      clientY: 0,
+    })
+  );
+  await act(async () => {});
+
+  expectLocation({x: 0, y: 0});
+});
+
+test('ignores mouse events when explicit coords are specified', async () => {
+  render(<App point={{x: 0, y: 0}} />);
+
+  fireEvent(
+    screen.getByTestId('reference'),
+    new MouseEvent('mousemove', {
+      bubbles: true,
+      clientX: 500,
+      clientY: 500,
+    })
+  );
+  await act(async () => {});
+
+  expectLocation({x: 0, y: 0});
+});
+
+test('cleans up window listener when closing or disabling', async () => {
+  const {rerender} = render(<App />);
+
+  fireEvent.click(screen.getByRole('button'));
+
+  fireEvent(
+    screen.getByTestId('reference'),
+    new MouseEvent('mousemove', {
+      bubbles: true,
+      clientX: 500,
+      clientY: 500,
+    })
+  );
+  await act(async () => {});
+
+  fireEvent.click(screen.getByRole('button'));
+
+  fireEvent(
+    document.body,
+    new MouseEvent('mousemove', {
+      bubbles: true,
+      clientX: 0,
+      clientY: 0,
+    })
+  );
+  await act(async () => {});
+
+  expectLocation({x: 500, y: 500});
+
+  fireEvent.click(screen.getByRole('button'));
+
+  fireEvent(
+    document.body,
+    new MouseEvent('mousemove', {
+      bubbles: true,
+      clientX: 500,
+      clientY: 500,
+    })
+  );
+  await act(async () => {});
+
+  expectLocation({x: 500, y: 500});
+
+  rerender(<App enabled={false} />);
+
+  fireEvent(
+    document.body,
+    new MouseEvent('mousemove', {
+      bubbles: true,
+      clientX: 0,
+      clientY: 0,
+    })
+  );
+  await act(async () => {});
+
+  expectLocation({x: 500, y: 500});
+});
+
+test('axis x', async () => {
+  render(<App axis="x" />);
+
+  fireEvent.click(screen.getByRole('button'));
+
+  fireEvent(
+    screen.getByTestId('reference'),
+    new MouseEvent('mousemove', {
+      bubbles: true,
+      clientX: 500,
+      clientY: 500,
+    })
+  );
+  await act(async () => {});
+
+  expectLocation({x: 500, y: 0});
+});
+
+test('axis y', async () => {
+  render(<App axis="y" />);
+
+  fireEvent.click(screen.getByRole('button'));
+
+  fireEvent(
+    screen.getByTestId('reference'),
+    new MouseEvent('mousemove', {
+      bubbles: true,
+      clientX: 500,
+      clientY: 500,
+    })
+  );
+  await act(async () => {});
+
+  expectLocation({x: 0, y: 500});
+});
+
+test('removes window listener when cursor lands on floating element', async () => {
+  render(<App />);
+
+  fireEvent.click(screen.getByRole('button'));
+
+  fireEvent(
+    screen.getByTestId('reference'),
+    new MouseEvent('mousemove', {
+      bubbles: true,
+      clientX: 500,
+      clientY: 500,
+    })
+  );
+
+  fireEvent(
+    screen.getByTestId('floating'),
+    new MouseEvent('mousemove', {
+      bubbles: true,
+      clientX: 500,
+      clientY: 500,
+    })
+  );
+
+  fireEvent(
+    document.body,
+    new MouseEvent('mousemove', {
+      bubbles: true,
+      clientX: 0,
+      clientY: 0,
+    })
+  );
+  await act(async () => {});
+
+  expectLocation({x: 500, y: 500});
+});
