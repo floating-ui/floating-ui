39d3c62d5364d02cc8fb5c4a255153348f382e32
atomiks <cc.glows@gmail.com>
2023-03-14 08:52:58 +1100

    fix(inline): group rects by line with fuzzy height and y coord (#2226)

--- packages/core/test/middleware/inline.test.ts
+++ packages/core/test/middleware/inline.test.ts
@@ -1,59 +1,52 @@
-import {mergeRects} from '../../src/middleware/inline';
+import {rectToClientRect} from '../../src';
+import {getRectsByLine} from '../../src/middleware/inline';
 
-test('merges rects on the same line (y coordinate)', () => {
-  expect(
-    mergeRects([
-      {
-        top: 0,
-        left: 0,
-        bottom: 10,
-        right: 10,
-        width: 10,
-        height: 10,
-        x: 0,
-        y: 0,
-      },
-      {
-        top: 0,
-        left: 10,
-        bottom: 10,
-        right: 30,
-        width: 20,
-        height: 10,
-        x: 10,
-        y: 0,
-      },
-      {
-        top: 10,
-        left: 0,
-        bottom: 20,
-        right: 10,
-        width: 10,
-        height: 10,
-        x: 0,
-        y: 10,
-      },
-    ])
-  ).toEqual([
-    {
-      top: 0,
-      left: 0,
-      bottom: 10,
-      right: 30,
-      width: 30,
-      height: 10,
-      x: 0,
-      y: 0,
-    },
-    {
-      top: 10,
-      left: 0,
-      bottom: 20,
-      right: 10,
-      width: 10,
-      height: 10,
-      x: 0,
-      y: 10,
-    },
-  ]);
+describe('getRectsByLine', () => {
+  test('single line', () => {
+    expect(
+      getRectsByLine([
+        rectToClientRect({x: 0, y: 0, width: 10, height: 10}),
+        rectToClientRect({x: 10, y: 0, width: 10, height: 10}),
+        rectToClientRect({x: 20, y: 0, width: 10, height: 10}),
+      ])
+    ).toEqual([rectToClientRect({x: 0, y: 0, width: 30, height: 10})]);
+  });
+
+  test('multiple lines', () => {
+    expect(
+      getRectsByLine([
+        rectToClientRect({x: 0, y: 0, width: 10, height: 10}),
+        rectToClientRect({x: 10, y: 0, width: 10, height: 10}),
+        rectToClientRect({x: 20, y: 0, width: 10, height: 10}),
+        rectToClientRect({x: 20, y: 10, width: 100, height: 10}),
+      ])
+    ).toEqual([
+      rectToClientRect({x: 0, y: 0, width: 30, height: 10}),
+      rectToClientRect({x: 20, y: 10, width: 100, height: 10}),
+    ]);
+  });
+
+  test('multiple lines, different heights and y coords', () => {
+    expect(
+      getRectsByLine([
+        rectToClientRect({x: 0, y: 0, width: 10, height: 10}),
+        rectToClientRect({x: 10, y: 3, width: 10, height: 8}),
+        rectToClientRect({x: 20, y: 1, width: 10, height: 5}),
+      ])
+    ).toEqual([rectToClientRect({x: 0, y: 0, width: 30, height: 11})]);
+  });
+
+  test('multiple lines, different heights and y coords, with a gap', () => {
+    expect(
+      getRectsByLine([
+        rectToClientRect({x: 0, y: 0, width: 10, height: 10}),
+        rectToClientRect({x: 10, y: 3, width: 10, height: 8}),
+        rectToClientRect({x: 20, y: 1, width: 10, height: 5}),
+        rectToClientRect({x: 20, y: 20, width: 10, height: 5}),
+      ])
+    ).toEqual([
+      rectToClientRect({x: 0, y: 0, width: 30, height: 11}),
+      rectToClientRect({x: 20, y: 20, width: 10, height: 5}),
+    ]);
+  });
 });
