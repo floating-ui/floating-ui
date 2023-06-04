25a2b52e17438b9a757b77c13d24a5a31575220b
atomiks <cc.glows@gmail.com>
2023-06-04 20:19:42 +1000

    pass from focus guard closeOnFocusOut

--- packages/react/src/components/FloatingPortal.tsx
+++ packages/react/src/components/FloatingPortal.tsx
@@ -205,7 +205,7 @@ export function FloatingPortal({
                 focusManagerState?.refs.domReference.current;
               nextTabbable?.focus();
               focusManagerState?.closeOnFocusOut &&
-                focusManagerState?.onOpenChange(false);
+                focusManagerState?.onOpenChange(false, event.nativeEvent);
             }
           }}
         />
