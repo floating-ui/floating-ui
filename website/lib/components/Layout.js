d2a9b87e6a29bf18a23e2de0efc3f40d8a034c63
atomiks <cc.glows@gmail.com>
2023-06-04 16:13:43 +1000

    docs: use svg clip-path for squircle

--- website/lib/components/Layout.js
+++ website/lib/components/Layout.js
@@ -704,6 +704,16 @@ export default function Layout({children, className}) {
         <title>{title}</title>
       </Head>
       <SkipNavLink />
+      <svg width="0" height="0">
+        <defs>
+          <clipPath
+            id="squircle"
+            clipPathUnits="objectBoundingBox"
+          >
+            <path d="M .5,0 C .1,0 0,.1 0,.5 0,.9 .1,1 .5,1 .9,1 1,.9 1,.5 1,.1 .9,0 .5,0 Z" />
+          </clipPath>
+        </defs>
+      </svg>
       <div
         className={`md:pl-64 lg:px-72 lg:pr-0 xl:px-[22rem] xl:pr-72 ${className}`}
       >
