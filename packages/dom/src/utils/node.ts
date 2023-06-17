9ecf79709bbe651d7a1438be6402b3a5dd3528dc
atomiks <cc.glows@gmail.com>
2023-06-17 10:45:50 +1000

    fix(dom): loop when Node is mocked

--- packages/dom/src/utils/node.ts
+++ packages/dom/src/utils/node.ts
@@ -5,5 +5,11 @@ export function isNode(value: any): value is Node {
 }
 
 export function getNodeName(node: Node | Window): string {
-  return isNode(node) ? (node.nodeName || '').toLowerCase() : '';
+  if (isNode(node)) {
+    return (node.nodeName || '').toLowerCase();
+  }
+  // Mocked nodes in testing environments may not be instances of Node. By
+  // returning `#document` an infinite loop won't occur.
+  // https://github.com/floating-ui/floating-ui/issues/2317
+  return '#document';
 }
