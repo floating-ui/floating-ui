3b13742d409da79490a27402365e43a0107730b3
atomiks <cc.glows@gmail.com>
2023-06-27 14:43:00 +1000

    docs: update

--- website/lib/components/WordHighlight.js
+++ website/lib/components/WordHighlight.js
@@ -4,9 +4,9 @@ export function WordHighlight({children, id}) {
   return (
     <span
       className={cn('rounded border-b p-1', {
-        'border-b-rose-300 bg-rose-100 text-rose-900 dark:bg-rose-800 dark:text-rose-200':
+        'border-b-rose-300 bg-rose-100 text-rose-900 dark:border-b-rose-500 dark:bg-rose-800 dark:text-rose-200':
           id === 'a',
-        'border-b-cyan-300 bg-cyan-100 text-cyan-900 dark:bg-cyan-800 dark:text-cyan-200':
+        'border-b-cyan-300 bg-cyan-100 text-cyan-900 dark:border-b-cyan-500 dark:bg-cyan-800 dark:text-cyan-200':
           id === 'b',
       })}
     >
