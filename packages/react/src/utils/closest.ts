b8b1937499d0afdc4f18de8f3090a4307a476874
atomiks <cc.glows@gmail.com>
2023-07-03 23:57:36 +1000

    ignore html/body match

--- packages/react/src/utils/closest.ts
+++ packages/react/src/utils/closest.ts
@@ -1,7 +1,9 @@
 import {isElement, isShadowRoot} from './is';
 
 export function closest(node: Node | null, selector: string): Node | null {
-  if (!node) return null;
+  if (!node || (isElement(node) && node.matches('html,body'))) {
+    return null;
+  }
 
   if (isShadowRoot(node)) {
     return closest(node.host, selector);
