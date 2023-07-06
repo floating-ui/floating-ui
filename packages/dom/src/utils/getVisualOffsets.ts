0fc96df8978c95ee0002800c0383e0e2c06cc57b
atomiks <cc.glows@gmail.com>
2023-07-06 12:25:14 +1000

    return types

--- packages/dom/src/utils/getVisualOffsets.ts
+++ packages/dom/src/utils/getVisualOffsets.ts
@@ -1,10 +1,12 @@
+import type {Coords} from '@floating-ui/core';
+
 import {getWindow} from './getWindow';
 import {isSafari} from './is';
 import {createCoords} from './math';
 
 const noOffsets = createCoords(0);
 
-export function getVisualOffsets(element: Element | undefined) {
+export function getVisualOffsets(element: Element | undefined): Coords {
   if (!isSafari()) {
     return noOffsets;
   }
@@ -21,7 +23,7 @@ export function shouldAddVisualOffsets(
   element: Element | undefined,
   isFixed = false,
   floatingOffsetParent?: Element | Window | undefined
-) {
+): boolean {
   if (
     !floatingOffsetParent ||
     (isFixed && floatingOffsetParent !== getWindow(element))
