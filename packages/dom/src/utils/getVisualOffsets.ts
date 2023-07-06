e75cef45d19577b80a7b7d5a9608d033a9bb337d
atomiks <cc.glows@gmail.com>
2023-07-06 12:02:10 +1000

    fix: positioning when pinch-zooming in Safari

--- packages/dom/src/utils/getVisualOffsets.ts
+++ packages/dom/src/utils/getVisualOffsets.ts
@@ -4,23 +4,29 @@ import {createCoords} from './math';
 
 const noOffsets = createCoords(0);
 
-export function getVisualOffsets(
-  element: Element | undefined,
-  isFixed = true,
-  floatingOffsetParent?: Element | Window | undefined
-) {
+export function getVisualOffsets(element: Element | undefined) {
   if (!isSafari()) {
     return noOffsets;
   }
 
-  const win = element ? getWindow(element) : window;
-
-  if (!floatingOffsetParent || (isFixed && floatingOffsetParent !== win)) {
-    return noOffsets;
-  }
+  const win = getWindow(element);
 
   return {
     x: win.visualViewport?.offsetLeft || 0,
     y: win.visualViewport?.offsetTop || 0,
   };
+}
+
+export function shouldAddVisualOffsets(
+  element: Element | undefined,
+  isFixed = false,
+  floatingOffsetParent?: Element | Window | undefined
+) {
+  const win = getWindow(element);
+
+  if (!floatingOffsetParent || (isFixed && floatingOffsetParent !== win)) {
+    return false;
+  }
+
+  return isFixed;
 }
