5fadd0013be67a22dbb49c465b201426f6a99630
atomiks <cc.glows@gmail.com>
2023-07-06 12:21:32 +1000

    refactor

--- packages/dom/src/utils/getWindow.ts
+++ packages/dom/src/utils/getWindow.ts
@@ -1,3 +1,3 @@
 export function getWindow(node: Node | undefined): Window {
-  return node ? node.ownerDocument?.defaultView || window : window;
+  return node?.ownerDocument?.defaultView || window;
 }
