8104704f65481070b244da66f1700633cf45de11
atomiks <cc.glows@gmail.com>
2023-08-08 09:19:33 +1000

    perf: shorten markOthers

--- packages/react/src/utils/markOthers.ts
+++ packages/react/src/utils/markOthers.ts
@@ -1,27 +1,21 @@
 // Modified to add conditional `aria-hidden` support:
 // https://github.com/theKashey/aria-hidden/blob/9220c8f4a4fd35f63bee5510a9f41a37264382d4/src/index.ts
+import {getDocument} from '@floating-ui/utils/react';
+
 type Undo = () => void;
 
 let counterMap = new WeakMap<Element, number>();
-let uncontrolledNodes = new WeakMap<Element, boolean>();
+let uncontrolledElementsSet = new WeakSet<Element>();
 let markerMap: Record<string, WeakMap<Element, number>> = {};
 let lockCount = 0;
 
 export const supportsInert = (): boolean =>
   typeof HTMLElement !== 'undefined' && 'inert' in HTMLElement.prototype;
 
-const getDefaultParent = (originalTarget: Element[]): HTMLElement | null => {
-  if (typeof document === 'undefined') {
-    return null;
-  }
-  const sampleTarget = originalTarget[0];
-  return sampleTarget.ownerDocument.body;
-};
-
 const unwrapHost = (node: Element | ShadowRoot): Element | null =>
   node && ((node as ShadowRoot).host || unwrapHost(node.parentNode as Element));
 
-const correctTargets = (parent: HTMLElement, targets: Element[]): Element[] =>
+const correctElements = (parent: HTMLElement, targets: Element[]): Element[] =>
   targets
     .map((target) => {
       if (parent.contains(target)) {
@@ -30,49 +24,47 @@ const correctTargets = (parent: HTMLElement, targets: Element[]): Element[] =>
 
       const correctedTarget = unwrapHost(target);
 
-      if (correctedTarget && parent.contains(correctedTarget)) {
+      if (parent.contains(correctedTarget)) {
         return correctedTarget;
       }
 
       return null;
     })
-    .filter((x): x is Element => Boolean(x));
+    .filter((x): x is Element => x != null);
 
-const applyAttributeToOthers = (
-  originalTarget: Element | Element[],
-  parentNode: HTMLElement,
+function applyAttributeToOthers(
+  uncorrectedAvoidElements: Element[],
+  body: HTMLElement,
   ariaHidden: boolean,
   inert: boolean
-): Undo => {
+): Undo {
   const markerName = 'data-floating-ui-inert';
   const controlAttribute = inert ? 'inert' : ariaHidden ? 'aria-hidden' : null;
-  const targets = correctTargets(
-    parentNode,
-    Array.isArray(originalTarget) ? originalTarget : [originalTarget]
-  );
+  const avoidElements = correctElements(body, uncorrectedAvoidElements);
+  const elementsToKeep = new Set<Node>();
+  const elementsToStop = new Set<Node>(avoidElements);
+  const hiddenElements: Element[] = [];
 
   if (!markerMap[markerName]) {
     markerMap[markerName] = new WeakMap();
   }
 
   const markerCounter = markerMap[markerName];
-  const hiddenNodes: Element[] = [];
 
-  const elementsToKeep = new Set<Node>();
-  const elementsToStop = new Set<Node>(targets);
+  avoidElements.forEach(keep);
+  deep(body);
+  elementsToKeep.clear();
 
-  const keep = (el: Node | undefined) => {
+  function keep(el: Node | undefined) {
     if (!el || elementsToKeep.has(el)) {
       return;
     }
 
     elementsToKeep.add(el);
     el.parentNode && keep(el.parentNode);
-  };
+  }
 
-  targets.forEach(keep);
-
-  const deep = (parent: Element | null) => {
+  function deep(parent: Element | null) {
     if (!parent || elementsToStop.has(parent)) {
       return;
     }
@@ -90,10 +82,10 @@ const applyAttributeToOthers = (
 
         counterMap.set(node, counterValue);
         markerCounter.set(node, markerValue);
-        hiddenNodes.push(node);
+        hiddenElements.push(node);
 
         if (counterValue === 1 && alreadyHidden) {
-          uncontrolledNodes.set(node, true);
+          uncontrolledElementsSet.add(node);
         }
 
         if (markerValue === 1) {
@@ -105,31 +97,28 @@ const applyAttributeToOthers = (
         }
       }
     });
-  };
-
-  deep(parentNode);
-  elementsToKeep.clear();
+  }
 
   lockCount++;
 
   return () => {
-    hiddenNodes.forEach((node) => {
-      const counterValue = (counterMap.get(node) || 0) - 1;
-      const markerValue = (markerCounter.get(node) || 0) - 1;
+    hiddenElements.forEach((element) => {
+      const counterValue = (counterMap.get(element) || 0) - 1;
+      const markerValue = (markerCounter.get(element) || 0) - 1;
 
-      counterMap.set(node, counterValue);
-      markerCounter.set(node, markerValue);
+      counterMap.set(element, counterValue);
+      markerCounter.set(element, markerValue);
 
       if (!counterValue) {
-        if (!uncontrolledNodes.has(node) && controlAttribute) {
-          node.removeAttribute(controlAttribute);
+        if (!uncontrolledElementsSet.has(element) && controlAttribute) {
+          element.removeAttribute(controlAttribute);
         }
 
-        uncontrolledNodes.delete(node);
+        uncontrolledElementsSet.delete(element);
       }
 
       if (!markerValue) {
-        node.removeAttribute(markerName);
+        element.removeAttribute(markerName);
       }
     });
 
@@ -138,31 +127,22 @@ const applyAttributeToOthers = (
     if (!lockCount) {
       counterMap = new WeakMap();
       counterMap = new WeakMap();
-      uncontrolledNodes = new WeakMap();
+      uncontrolledElementsSet = new WeakSet();
       markerMap = {};
     }
   };
-};
+}
 
-export const markOthers = (
+export function markOthers(
   avoidElements: Element[],
   ariaHidden = false,
   inert = false
-): Undo => {
-  const activeParentNode = getDefaultParent(avoidElements);
-
-  if (!activeParentNode) {
-    return () => null;
-  }
-
-  avoidElements.push(
-    ...Array.from(activeParentNode.querySelectorAll('[aria-live]'))
-  );
-
+): Undo {
+  const body = getDocument(avoidElements[0]).body;
   return applyAttributeToOthers(
-    avoidElements,
-    activeParentNode,
+    avoidElements.concat(Array.from(body.querySelectorAll('[aria-live]'))),
+    body,
     ariaHidden,
     inert
   );
-};
+}
