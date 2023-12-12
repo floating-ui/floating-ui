b50b20d64b48db6513a3b3f32e03b810e8efaf3e
madaxen86 <59310516+madaxen86@users.noreply.github.com>
2023-12-12 17:08:00 +0100

    es-lint rule

--- packages/solid/.eslintrc.cjs
+++ packages/solid/.eslintrc.cjs
@@ -32,6 +32,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/consistent-type-imports': 'off',
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
     'simple-import-sort/imports': 'error',
