c5ff73a110233e933e3549f3454fcd0233af4980
atomiks <cc.glows@gmail.com>
2024-01-06 15:29:29 +1100

    test: fix act warnings

--- packages/react/test/unit/FloatingDelayGroup.test.tsx
+++ packages/react/test/unit/FloatingDelayGroup.test.tsx
@@ -1,5 +1,4 @@
-import {fireEvent, render, screen} from '@testing-library/react';
-import {act} from '@testing-library/react-hooks';
+import {act, fireEvent, render, screen} from '@testing-library/react';
 import {cloneElement, useState} from 'react';
 import {vi} from 'vitest';
 
