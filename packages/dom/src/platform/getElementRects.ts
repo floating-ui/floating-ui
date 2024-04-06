84c436d51e4c1a213a96bfcb775e65eb2301d759
atomiks <cc.glows@gmail.com>
2024-04-06 16:37:45 +1100

    fix(getElementRects): avoid spreading floating dimensions

--- packages/dom/src/platform/getElementRects.ts
+++ packages/dom/src/platform/getElementRects.ts
@@ -8,12 +8,19 @@ export const getElementRects: Platform['getElementRects'] = async function (
 ) {
   const getOffsetParentFn = this.getOffsetParent || getOffsetParent;
   const getDimensionsFn = this.getDimensions;
+  const floatingDimensions = await getDimensionsFn(data.floating);
+
   return {
     reference: getRectRelativeToOffsetParent(
       data.reference,
       await getOffsetParentFn(data.floating),
       data.strategy,
     ),
-    floating: {x: 0, y: 0, ...(await getDimensionsFn(data.floating))},
+    floating: {
+      x: 0,
+      y: 0,
+      width: floatingDimensions.width,
+      height: floatingDimensions.height,
+    },
   };
 };
