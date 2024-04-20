5dd571815d67cc1b1adf89a83699f6e3bee3efb5
atomiks <cc.glows@gmail.com>
2024-04-20 17:00:09 +1000

    fix: OffsetOptions

--- packages/dom/src/middleware.ts
+++ packages/dom/src/middleware.ts
@@ -32,11 +32,34 @@ import type {
 
 // `OffsetOptions` in the core library were originally already `Derivable`. For
 // backwards-compatibility, re-define it here to use the DOM Derivable type.
-type OffsetOptions = {
-  mainAxis?: number;
-  crossAxis?: number;
-  alignmentAxis?: number | null;
-};
+type OffsetOptions =
+  | number
+  | Partial<{
+      /**
+       * The axis that runs along the side of the floating element. Represents
+       * the distance (gutter or margin) between the reference and floating
+       * element.
+       * @default 0
+       */
+      mainAxis: number;
+      /**
+       * The axis that runs along the alignment of the floating element.
+       * Represents the skidding between the reference and floating element.
+       * @default 0
+       */
+      crossAxis: number;
+      /**
+       * The same axis as `crossAxis` but applies only to aligned placements
+       * and inverts the `end` alignment. When set to a number, it overrides the
+       * `crossAxis` value.
+       *
+       * A positive number will move the floating element in the direction of
+       * the opposite edge to the one that is aligned, while a negative number
+       * the reverse.
+       * @default null
+       */
+      alignmentAxis: number | null;
+    }>;
 
 /**
  * Resolves with an object of overflow side offsets that determine how much the
