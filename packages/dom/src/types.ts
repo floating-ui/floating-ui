8e613d71797329157b8107aca0e4a7ab3a006564
atomiks <cc.glows@gmail.com>
2024-05-05 11:07:08 +1000

    fix(types): correct `OffsetOptions` alias (#2888)

--- packages/dom/src/types.ts
+++ packages/dom/src/types.ts
@@ -26,6 +26,38 @@ type Promisable<T> = T | Promise<T>;
 
 export type Derivable<T> = (state: MiddlewareState) => T;
 
+export type OffsetValue =
+  | number
+  | {
+      /**
+       * The axis that runs along the side of the floating element. Represents
+       * the distance (gutter or margin) between the reference and floating
+       * element.
+       * @default 0
+       */
+      mainAxis?: number;
+      /**
+       * The axis that runs along the alignment of the floating element.
+       * Represents the skidding between the reference and floating element.
+       * @default 0
+       */
+      crossAxis?: number;
+      /**
+       * The same axis as `crossAxis` but applies only to aligned placements
+       * and inverts the `end` alignment. When set to a number, it overrides the
+       * `crossAxis` value.
+       *
+       * A positive number will move the floating element in the direction of
+       * the opposite edge to the one that is aligned, while a negative number
+       * the reverse.
+       * @default null
+       */
+      alignmentAxis?: number | null;
+    };
+// `OffsetOptions` in the core library were originally already `Derivable`. For
+// backwards-compatibility, re-define it here to use the DOM Derivable type.
+export type OffsetOptions = OffsetValue | Derivable<OffsetValue>;
+
 export interface Platform {
   // Required
   getElementRects: (args: {
