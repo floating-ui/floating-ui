8a66e2fca98643bfcea4ce5e2d8609fd30f15d5d
atomiks <cc.glows@gmail.com>
2024-09-12 17:01:15 +1000

    tests

--- packages/dom/test/functional/relative.test.ts
+++ packages/dom/test/functional/relative.test.ts
@@ -12,4 +12,15 @@ import {click} from './utils/click';
       `${node}.png`,
     );
   });
+
+  test(`correctly positioned on bottom when ${node} is an offsetParent with an offset of -100px`, async ({
+    page,
+  }) => {
+    await page.goto('http://localhost:1234/relative');
+    await click(page, `[data-testid="relative-${node}"]`);
+    await click(page, `[data-testid="offset-100"]`);
+    expect(await page.locator('.container').screenshot()).toMatchSnapshot(
+      `${node}.png`,
+    );
+  });
 });
