229c5f71903e933fefe1f98f2457cf1210b8c960
atomiks <cc.glows@gmail.com>
2024-10-31 19:12:03 +1100

    fix(inner): use list ref length for fallback detection (#3101)

--- packages/react/src/inner.ts
+++ packages/react/src/inner.ts
@@ -175,8 +175,9 @@ export const inner = (
     // There is not enough space, fallback to standard anchored positioning
     if (onFallbackChange) {
       const shouldFallback =
-        (isScrollable &&
-          scrollEl.offsetHeight < item.offsetHeight * minItemsVisible - 1) ||
+        scrollEl.offsetHeight <
+          item.offsetHeight * min(minItemsVisible, listRef.current.length) -
+            1 ||
         refOverflow.top >= -referenceOverflowThreshold ||
         refOverflow.bottom >= -referenceOverflowThreshold;
 
