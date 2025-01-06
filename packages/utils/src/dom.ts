e72497581400a1333ab778126ca468638e66edf9
atomiks <cc.glows@gmail.com>
2025-01-07 09:04:36 +1100

    fix: prettier

--- packages/utils/src/dom.ts
+++ packages/utils/src/dom.ts
@@ -94,14 +94,17 @@ export function isContainingBlock(
   // https://developer.mozilla.org/en-US/docs/Web/CSS/Containing_block#identifying_the_containing_block
   // https://drafts.csswg.org/css-transforms-2/#individual-transforms
   return (
-    ['transform', 'translate', 'scale', 'rotate', 'perspective'].some((value) =>
-      css[value as keyof CSSStyleDeclaration] ? css[value as keyof CSSStyleDeclaration] !== 'none' : false
+    ['transform', 'translate', 'scale', 'rotate', 'perspective'].some(
+      (value) =>
+        css[value as keyof CSSStyleDeclaration]
+          ? css[value as keyof CSSStyleDeclaration] !== 'none'
+          : false,
     ) ||
     (css.containerType ? css.containerType !== 'normal' : false) ||
     (!webkit && (css.backdropFilter ? css.backdropFilter !== 'none' : false)) ||
     (!webkit && (css.filter ? css.filter !== 'none' : false)) ||
-    ['transform', 'translate', 'scale', 'rotate', 'perspective', 'filter'].some((value) =>
-      (css.willChange || '').includes(value),
+    ['transform', 'translate', 'scale', 'rotate', 'perspective', 'filter'].some(
+      (value) => (css.willChange || '').includes(value),
     ) ||
     ['paint', 'layout', 'strict', 'content'].some((value) =>
       (css.contain || '').includes(value),
